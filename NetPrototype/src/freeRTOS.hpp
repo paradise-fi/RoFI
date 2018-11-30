581dd28741bd0952b57e960c5f87273918f7a1c6
Jan Mrázek <email@honzamrazek.cz>
2018-12-01 00:09:13 +0100

    NetPrototype: Implement InterruptDeferrer

--- NetPrototype/src/freeRTOS.hpp
+++ NetPrototype/src/freeRTOS.hpp
@@ -191,4 +191,56 @@ public:
 private:
     SemaphoreHandle_t _sem;
 };
+
+class IsrDeferrer {
+    using Arg = void*;
+    using Handler = void (*)( Arg );
+public:
+    IsrDeferrer( int size )
+        : _q( xQueueCreate( size, sizeof( Handler ) + sizeof( Arg ) ) )
+    {
+        if ( !_q )
+            throw std::runtime_error( "Cannot allocate queue" );
+        auto res = xTaskCreate( _run, "IsrDeferrer", 2048, this, 15, nullptr );
+        if ( res != pdPASS )
+            throw std::runtime_error( "Cannot allocate task" );
+    }
+    IsrDeferrer( const IsrDeferrer& ) = delete;
+    IsrDeferrer& operator=( const IsrDeferrer& ) = delete;
+    IsrDeferrer( IsrDeferrer&& o ) : _q( o._q ) { o._q = nullptr; }
+    IsrDeferrer& operator=( IsrDeferrer&& o ) { swap( o ); return *this; }
+
+    ~IsrDeferrer() {
+        if ( _q )
+            vQueueDelete( _q );
+    }
+
+    void swap( IsrDeferrer& o ) {
+        using std::swap;
+        swap( _q, o._q );
+    }
+
+    void IRAM_ATTR isr( Arg a, Handler h ) {
+        uint8_t data[ sizeof( Handler ) + sizeof( Arg ) ];
+        *reinterpret_cast< Arg* >( data ) = a;
+        *reinterpret_cast< Handler* >( data + sizeof( Arg ) ) = h;
+        portBASE_TYPE higherPriorityTaskWoken = pdFALSE;
+        xQueueSendToBackFromISR( _q, data, &higherPriorityTaskWoken );
+        if( higherPriorityTaskWoken )
+            portYIELD_FROM_ISR();
+    }
+private:
+    static void _run(void* arg ) {
+        auto* self = reinterpret_cast< IsrDeferrer* >( arg );
+        while ( true ) {
+            uint8_t data[ sizeof( Handler ) + sizeof( Arg ) ];
+            xQueueReceive( self->_q, data, portMAX_DELAY );
+            Arg& a = *reinterpret_cast< Arg* >( data );
+            Handler& h = *reinterpret_cast< Handler* >( data + sizeof( Arg ) );
+            (*h)( a );
+        }
+    }
+    QueueHandle_t _q;
+};
+
 } // namespace rtos
