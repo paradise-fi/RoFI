d49213fc72fd735994205b5ec6b0bbc19de281f1
Markéta Naušová <xnausova@fi.muni.cz>
2019-03-14 19:27:16 +0100

    RoFILib: simple main and some results

--- RoFILib/visualizer/main.cpp
+++ RoFILib/visualizer/main.cpp
@@ -3,9 +3,40 @@
 #include <string_view>
 #include "../Reader.h"
 #include "Visualizer.h"
+#include "AnimationReader.h"
+#include "Animator.h"
+
+int mainWithAnimation(){
+    AnimationReader animReader;
+    animReader.read("../data/testMove.in");
+}
+
+int mainAnimator(){
+    Reader reader;
+    std::ifstream file;
+    file.open("../data/res.out");
+    std::vector<Configuration> configs;
+    reader.read(file, configs);
+    Camera s;
+    Camera e;
+    std::ifstream cam;
+    cam.open("../data/1.cam");
+    Animator animator;
+    bool cameraMove;
+    reader.readCameraSettings(cam, s, e, cameraMove);
+    animator.visualizeMainConfigs(configs, 30, 6, "../data", true, s, e);
+
+}
 
 int main(int argc, char *argv[])
 {
+/*
+    mainAnimator();
+    return 0;
+
+    mainWithAnimation();
+    return 0;
+*/
     Reader reader;
     Visualizer vis;
 
