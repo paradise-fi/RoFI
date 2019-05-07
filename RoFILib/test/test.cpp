b6cd7f1299b78c6e365ecec255e65761133fdbe0
Markéta Naušová <xnausova@fi.muni.cz>
2019-05-07 11:29:02 +0200

    RoFILib: Add simple test for visualizer/Generator

--- RoFILib/test/test.cpp
+++ RoFILib/test/test.cpp
@@ -2,6 +2,7 @@
 #include "../Configuration.h"
 #include "../visualizer/Visualizer.h"
 #include "../reconfig/Algorithms.h"
+#include "../visualizer/Generator.h"
 
 TEST_CASE("Connections")
 {
@@ -243,4 +244,139 @@ TEST_CASE("Diff reconnections and rotations") {
     REQUIRE((action.reconnections.at(0) == reconnect1 || action.reconnections.at(0) == reconnect2));
     REQUIRE((action.reconnections.at(1) == reconnect1 || action.reconnections.at(1) == reconnect2));
     REQUIRE(action.reconnections.at(0) != action.reconnections.at(1));
+}
+
+TEST_CASE("Visualizer simple generate"){
+    Configuration cfg1, cfg2;
+    cfg1.addModule(0,0,0, 0);
+    cfg1.addModule(0,0,0, 1);
+    cfg1.addModule(0,0,0, 2);
+    Edge e1(0, static_cast<Side>(0), static_cast<Dock>(0), 2, static_cast<Dock>(1), static_cast<Side>(0), 1);
+    Edge e2(1, static_cast<Side>(0), static_cast<Dock>(0), 2, static_cast<Dock>(1), static_cast<Side>(0), 2);
+    cfg1.addEdge(e1);
+    cfg1.addEdge(e2);
+
+
+    cfg2.addModule(0,90,0, 0);
+    cfg2.addModule(0,10,0, 1);
+    cfg2.addModule(0,0,0, 2);
+    Edge e3(0, static_cast<Side>(0), static_cast<Dock>(0), 2, static_cast<Dock>(1), static_cast<Side>(0), 1);
+    Edge e4(1, static_cast<Side>(0), static_cast<Dock>(0), 2, static_cast<Dock>(1), static_cast<Side>(0), 2);
+    cfg2.addEdge(e3);
+    cfg2.addEdge(e4);
+
+    SECTION("Only angle change") {
+        std::vector<Configuration> generatedConfigs;
+        double maxPhi = 30;
+        unsigned int reconnection = 8;
+        Generator generator;
+        generator.generate(cfg1, cfg2, generatedConfigs, maxPhi, reconnection);
+        REQUIRE(generatedConfigs.size() == 2);
+
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Beta) == 30);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Beta) == 60);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Gamma) == 0);
+
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Gamma) == 0);
+    }
+
+    SECTION("Add edge") {
+        Edge e5(1, static_cast<Side>(1), static_cast<Dock>(1), 2, static_cast<Dock>(0), static_cast<Side>(1), 2);
+        cfg2.addEdge(e5);
+        std::vector<Configuration> generatedConfigs;
+        double maxPhi = 30;
+        unsigned int reconnection = 8;
+        Generator generator;
+        generator.generate(cfg1, cfg2, generatedConfigs, maxPhi, reconnection);
+        REQUIRE(generatedConfigs.size() == 7);
+
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Beta) == 30);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Beta) == 60);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Gamma) == 0);
+
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Gamma) == 0);
+
+        for (int i : {2, 3, 4, 5, 6}){
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Alpha) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Beta) == 90);
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Gamma) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Alpha) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Beta) == 10);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Gamma) == 0);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            std::vector<Edge> edges = generatedConfigs[i].getEdges(1);
+            for (const auto &e : edges) {
+                if (e == e5) {
+                    REQUIRE(e.onCoeff() == 1/static_cast<double>(8) * (i + 1));
+                } else {
+                    REQUIRE(e.onCoeff() == 1);
+                }
+            }
+        }
+    }
+
+    SECTION("Remove edge"){
+        Edge e5(1, static_cast<Side>(1), static_cast<Dock>(1), 2, static_cast<Dock>(0), static_cast<Side>(1), 2);
+        cfg1.addEdge(e5);
+        std::vector<Configuration> generatedConfigs;
+        double maxPhi = 30;
+        unsigned int reconnection = 8;
+        Generator generator;
+        generator.generate(cfg1, cfg2, generatedConfigs, maxPhi, reconnection);
+        REQUIRE(generatedConfigs.size() == 7);
+
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Beta) == 30);
+        REQUIRE(generatedConfigs[0].getModules().at(0).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Beta) == 60);
+        REQUIRE(generatedConfigs[1].getModules().at(0).getJoint(Gamma) == 0);
+
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[0].getModules().at(1).getJoint(Gamma) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Alpha) == 0);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Beta) == 10);
+        REQUIRE(generatedConfigs[1].getModules().at(1).getJoint(Gamma) == 0);
+
+        for (int i : {2, 3, 4, 5, 6}){
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Alpha) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Beta) == 90);
+            REQUIRE(generatedConfigs[i].getModules().at(0).getJoint(Gamma) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Alpha) == 0);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Beta) == 10);
+            REQUIRE(generatedConfigs[i].getModules().at(1).getJoint(Gamma) == 0);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            std::vector<Edge> edges = generatedConfigs[i].getEdges(1);
+            for (const auto &e : edges) {
+                if (e == e5) {
+                    REQUIRE(e.onCoeff() == 1 - 1/static_cast<double>(8) * (i + 1));
+                } else {
+                    REQUIRE(e.onCoeff() == 1);
+                }
+            }
+        }
+    }
+
+
 }
