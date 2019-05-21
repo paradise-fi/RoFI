2b2b559b42aabc97274648b00f0523e353a8f768
Jan Mrázek <email@honzamrazek.cz>
2019-05-21 16:12:13 +0200

    RoFICoM: Implement the Every handler

--- RoFICoM/software/control_board/src/util.hpp
+++ RoFICoM/software/control_board/src/util.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stm32g0xx_hal.h>
+#include <function2/function2.hpp>
 #include <cstring>
 
 // ARM can silently round-down unaligned acess to memory Therefore, naive
@@ -27,3 +29,30 @@ template < typename T >
 AsProxy< T > viewAs( void *ptr ) {
     return ptr;
 }
+
+class Every {
+public:
+    using Job =  fu2::unique_function< void() >;
+
+    Every( uint32_t interval, Job j )
+        : _interval( interval ),
+          _next( HAL_GetTick() + _interval ),
+          _do( std::move( j ) )
+    {}
+
+    void operator()() {
+        if ( HAL_GetTick() < _next )
+            return;
+        _next += _interval;
+        _do();
+    }
+
+    void reset() {
+        _next = HAL_GetTick() + _interval;
+    }
+
+private:
+    uint32_t _interval;
+    uint32_t _next;
+    Job _do;
+};
