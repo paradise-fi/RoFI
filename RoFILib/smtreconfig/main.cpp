a55a944a04eecd537b7881a3bc9e312da62702a6
Jan Mrázek <email@honzamrazek.cz>
2019-08-28 13:40:35 +0200

    RoFILib: Add project template for smt-reconfig

---
+++ RoFILib/smtreconfig/main.cpp
@@ -0,0 +1,30 @@
+#include <z3++.h>
+
+bool doesDeMorganHold() {
+    z3::context c;
+
+    auto x = c.bool_const("x");
+    auto y = c.bool_const("y");
+    auto conjecture = (!(x && y)) == (!x || !y);
+
+    z3::solver s(c);
+    s.add(!conjecture);
+
+    std::cout << "Conjecture: " << s << "\n";
+    switch (s.check()) {
+    case z3::unsat:
+        std::cout << "de-Morgan is valid\n";
+        return true;
+    case z3::sat:
+        std::cout << "de-Morgan is not valid\n";
+        return false;
+    case z3::unknown:
+        std::cout << "unknown\n";
+        return false;
+    }
+    __builtin_unreachable();
+}
+
+int main() {
+    doesDeMorganHold();
+}
