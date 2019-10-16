aa2a6ada5b4869fb93f47b4764242cea81d7d3b7
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2019-10-16 15:09:11 +0200

    RoFISim: Add command line client

--- simulator/plugins/universalModule/main.cpp
+++ simulator/plugins/universalModule/main.cpp
@@ -3,13 +3,26 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <iostream>
+#include <array>
 
-#include <jointPosCmd.pb.h>
-#include <jointVelCmd.pb.h>
+#include <jointCmd.pb.h>
 
 namespace gazebo {
 
-using JointVelCmdPtr = const boost::shared_ptr< const rofi::messages::JointVelCmd >;
+template <typename T>
+std::string to_string(const ignition::math::Vector3<T> &v)
+{
+    using std::to_string;
+    return "X = " + to_string(v.X()) + ", Y = " + to_string(v.Y()) + ", Z = " + to_string(v.Z());
+}
+
+std::string to_string(const gazebo::physics::JointWrench &jw)
+{
+    return "Forces:\n\tbody1: " + to_string(jw.body1Force) + "\n\tbody2: " + to_string(jw.body2Force)
+    + "\nTorques:\n\tbody1: " + to_string(jw.body1Torque) + "\n\tbody2: " + to_string(jw.body2Torque);
+}
+
+using JointCmdPtr = const boost::shared_ptr< const rofi::messages::JointCmd >;
 
 class UniversalModulePlugin : public ModelPlugin {
 public:
@@ -22,30 +35,58 @@ public:
         std::cerr << "\nThe UM plugin is attached to model ["
                 << model->GetName() << "]\n";
 
+        std::cerr << "Number of joints is " << model->GetJoints().size() << "\n";
         _model = model;
-        _joint = model->GetJoints()[0];
-        _model->GetJointController()->SetVelocityPID(_joint->GetScopedName(), _pid);
-        _model->GetJointController()->SetVelocityTarget(_joint->GetScopedName(), 10.0);
+        auto _bodyJoint = _model->GetJoint("bodyRev");
+        auto _shoeAJoint = _model->GetJoint("shoeARev");
+        auto _shoeBJoint = _model->GetJoint("shoeBRev");
+        if (!_bodyJoint || !_shoeAJoint || !_shoeBJoint) {
+            std::cerr << "\nCould not get all joints\n";
+            return;
+        }
+
+        for (auto &joint : {_shoeAJoint, _shoeBJoint})
+        {
+            joint->SetParam("fmax", 0, 100.0);
+            joint->SetParam("vel", 0, 0.0);
+        }
+
+        _bodyJoint->SetParam("fmax", 0, 100.0);
+        _bodyJoint->SetParam("vel", 0, 10.0);
 
         _node->Init(_model->GetWorld()->Name());
         std::string topicName = "~/" + _model->GetName() + "/control";
-        _topic = _node->Subscribe(topicName, &UniversalModulePlugin::onJointVelCmd, this);
+        _topic = _node->Subscribe(topicName, &UniversalModulePlugin::onJointCmd, this);
     }
 private:
-    void onJointVelCmd( JointVelCmdPtr &msg ) {
+    void onJointCmd( JointCmdPtr &msg )
+    {
         auto& joints = _model->GetJoints();
-        if ( msg->joint() < joints.size() ) {
-            _model->GetJointController()->SetVelocityTarget(
-                joints[ msg->joint() ]->GetScopedName(), msg->velocity());
-            std::cerr << "Setting velocity of joint " << msg->joint() << " to " << msg->velocity() << "\n";
-        }
-        else {
+        if ( msg->joint() >= joints.size() || msg->joint() < 0 )
+        {
             std::cerr << "Warning: invalid joint " << msg->joint() << " specified\n";
+            return;
+        }
+
+        switch ( msg->cmdtype() )
+        {
+            using JointCmd = rofi::messages::JointCmd;
+
+            case JointCmd::SET_TORQUE:
+            {
+                auto velocity = msg->settorque().torque();
+
+                joints[ msg->joint() ]->SetParam("vel", 0, velocity);
+                std::cerr << "Setting velocity of joint " << msg->joint() << " to " << velocity << "\n";
+                std::cerr << to_string(joints[ msg->joint() ]->GetForceTorque(0)) << "\n";
+                break;
+            }
+            // default:
+            //    std::cerr << "Unknown command type: " << msg->cmdtype() << "\n";
         }
     }
 
     physics::ModelPtr _model;
-    physics::JointPtr _joint;
     common::PID _pid;
     transport::NodePtr _node;
     transport::SubscriberPtr _topic;
--- simulator/tools/publisherDemo.cpp
+++ simulator/tools/publisherDemo.cpp
@@ -3,32 +3,212 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/gazebo_client.hh>
 
-#include <jointVelCmd.pb.h>
+#include <jointCmd.pb.h>
 
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
 
-int main(int argc, char **argv) {
-    try {
-        gazebo::client::setup(argc, argv);
 
-        gazebo::transport::NodePtr node(new gazebo::transport::Node());
+#define CONNECT 1 // Want to connect to gazebo?
+
+
+inline std::string_view ltrim( std::string_view line, std::string_view ws = " \t" )
+{
+    auto first = line.find_first_not_of( ws );
+    return first == std::string_view::npos ? "" : line.substr( first );
+}
+
+inline std::string_view rtrim( std::string_view line, std::string_view ws = " \t" )
+{
+    auto last = line.find_last_not_of( ws );
+    return last == std::string_view::npos ? "" : line.substr( 0, last + 1 );
+}
+
+inline std::string_view trim( std::string_view line, std::string_view ws = " \t" )
+{
+    return rtrim( ltrim( line, ws ), ws );
+}
+
+inline std::vector< std::string_view > split( std::string_view line )
+{
+    const std::string_view ws = " \t";
+
+    line = trim( line, ws );
+    std::vector< std::string_view > tokens;
+    while ( !line.empty() && line.front() != '#' )
+    {
+        auto last = line.find_first_of( ws );
+        tokens.push_back( line.substr( 0, last ) );
+
+        auto nextFirst = line.find_first_not_of( ws, last );
+        if ( nextFirst == std::string_view::npos )
+        {
+            break;
+        }
+
+        line.remove_prefix( nextFirst );
+    }
+
+    return tokens;
+}
+
+float readFloat( std::string_view str )
+{
+    return std::atof( std::string( str ).data() );
+}
+
+int readInt( std::string_view str )
+{
+    return std::atoi( std::string( str ).data() );
+}
+
+
+inline rofi::messages::JointCmd::Type getJointCmdType( std::string_view token )
+{
+    using rofi::messages::JointCmd;
+
+    const std::map< std::string_view, JointCmd::Type > map = {
+        { "getmaxposition", JointCmd::GET_MAX_POSITION },
+        { "getminposition", JointCmd::GET_MIN_POSITION },
+        { "getmaxspeed", JointCmd::GET_MAX_SPEED },
+        { "getminspeed", JointCmd::GET_MIN_SPEED },
+        { "getmaxtorque", JointCmd::GET_MAX_TORQUE },
+        { "getposition", JointCmd::GET_POSITION },
+        { "getspeed", JointCmd::GET_SPEED },
+        { "gettorque", JointCmd::GET_TORQUE },
+        { "setposwithspeed", JointCmd::SET_POS_WITH_SPEED },
+        { "setspeed", JointCmd::SET_SPEED },
+        { "settorque", JointCmd::SET_TORQUE }
+    };
+
+    auto it = map.find( token );
+    if ( it == map.end() )
+        throw std::runtime_error("Unknown joint cmd type");
+    return it->second;
+}
+
+void printHelp()
+{
+    std::cerr << "Help not yet implemented\n  write 'quit' for quit\nExample of usage:\n"
+              << "\tjoint 2 setspeed 8.5\n"
+              << "\tjoint 1 settorque -3.4\n"
+              << "\tjoint 2 getmaxposition\n";
+}
+
+rofi::messages::JointCmd processJointCmd( const std::vector< std::string_view > & tokens )
+{
+    using rofi::messages::JointCmd;
+
+    if ( tokens.size() < 3 )
+        throw std::runtime_error("Wrong number of arguments");
+
+    rofi::messages::JointCmd jointCmd;
+    jointCmd.set_joint( readInt( tokens[1] ) );
+
+    auto jointCmdType = getJointCmdType( tokens[2] );
+    jointCmd.set_cmdtype( jointCmdType );
+
+    switch ( jointCmdType )
+    {
+        case JointCmd::SET_POS_WITH_SPEED:
+        {
+            if ( tokens.size() != 5 )
+                throw std::runtime_error("Wrong number of arguments");
+
+            jointCmd.mutable_setposwithspeed()->set_position( readFloat( tokens[3] ) );
+            jointCmd.mutable_setposwithspeed()->set_speed( readFloat( tokens[4] ) );
+            break;
+        }
+        case JointCmd::SET_SPEED:
+        {
+            if ( tokens.size() != 4 )
+                throw std::runtime_error("Wrong number of arguments");
+
+            jointCmd.mutable_setspeed()->set_speed( readFloat( tokens[3] ) );
+            break;
+        }
+        case JointCmd::SET_TORQUE:
+        {
+            if ( tokens.size() != 4 )
+                throw std::runtime_error("Wrong number of arguments");
+
+            jointCmd.mutable_settorque()->set_torque( readFloat( tokens[3] ) );
+            break;
+        }
+        default:
+            if ( tokens.size() != 3 )
+                throw std::runtime_error("Wrong number of arguments");
+            break;
+    }
+
+    return jointCmd;
+}
+
+
+int main( int argc, char **argv )
+{
+    try
+    {
+#if CONNECT
+        if ( !gazebo::client::setup( argc, argv ) )
+        {
+            std::cerr << "Error on gazebo client setup\n";
+        }
+
+        gazebo::transport::NodePtr node( new gazebo::transport::Node() );
         node->Init();
 
-        gazebo::transport::PublisherPtr pub =
-            node->Advertise<rofi::messages::JointVelCmd>("~/universalModule/control");
+        auto pub = node->Advertise< rofi::messages::JointCmd >( "~/universalModule/control" );
+        if ( !pub )
+        {
+            std::cerr << "No found publisher\n";
+        }
 
         std::cerr << "Waiting for connection...\n";
         pub->WaitForConnection();
         std::cerr << "Connected\n";
+#endif
 
-        rofi::messages::JointVelCmd cmd;
-        cmd.set_joint(0);
-        cmd.set_velocity(std::atof(argv[1]));
+        for ( std::string line; std::getline( std::cin, line ); )
+        {
+            auto tokens = split( line );
+            if ( tokens.empty() )
+                continue;
 
-        pub->Publish(cmd);
+            if ( tokens.front() == "quit" )
+                break;
+
+            try
+            {
+                if ( tokens.front() == "joint" )
+                {
+                    auto msg = processJointCmd( tokens );
+#if CONNECT
+                    pub->Publish( msg );
+#else
+                    std::cout << "Sending...\n" << msg.DebugString();
+#endif
+                    continue;
+                }
+            }
+            catch ( const std::exception & e )
+            {
+                std::cerr << e.what() << '\n';
+                continue;
+            }
+
+            std::cerr << "Not yet implemented\n\n";
+            printHelp();
+        }
+
+#if CONNECT
         gazebo::client::shutdown();
+#endif
     }
-    catch( const gazebo::common::Exception& e ) {
+    catch ( const gazebo::common::Exception & e )
+    {
         std::cerr << e.GetErrorStr() << "\n";
     }
 }
