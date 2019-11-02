d79a22da48013bae8689480a60587ca8708ecc23
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2019-11-02 15:39:19 +0100

    RoFISim: Add static test of copy/move specification

---
+++ simulator/tools/test/test_static_rofi_hal.hpp
@@ -0,0 +1,16 @@
+#include "rofi_hal.hpp"
+
+#include <type_traits>
+
+static_assert( !std::is_default_constructible_v< rofi::hal::RoFI > );
+
+static_assert( !std::is_copy_constructible_v< rofi::hal::RoFI > );
+static_assert( !std::is_copy_assignable_v< rofi::hal::RoFI > );
+
+static_assert( std::is_move_constructible_v< rofi::hal::RoFI > );
+static_assert( std::is_move_assignable_v< rofi::hal::RoFI > );
+
+
+static_assert( !std::is_constructible_v< rofi::hal::RoFI::Joint > );
+static_assert( std::is_copy_constructible_v< rofi::hal::RoFI::Joint > );
+static_assert( std::is_copy_assignable_v< rofi::hal::RoFI::Joint > );
