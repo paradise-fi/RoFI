f648901c87904b90246a231be15c328d8dafa14d
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2020-03-30 12:52:04 +0200

    RoFISim: Update velocity PID and add basic joint velocity example

---
+++ simulator/examples/basic_joint_velocity/basic_joint_velocity.cpp
@@ -0,0 +1,36 @@
+#include "rofi_hal.hpp"
+
+#include <iostream>
+#include <future>
+
+
+int main()
+{
+    using namespace rofi::hal;
+
+    std::cout << "Starting basic velocity example\n";
+
+    RoFI & localRofi = RoFI::getLocalRoFI();
+    auto speed = 0.5 * localRofi.getJoint( 0 ).maxSpeed();
+
+    while ( true )
+    {
+        std::promise< void > endCyclePromise;
+        auto endCycleFuture = endCyclePromise.get_future();
+
+        localRofi.getJoint( 0 ).setVelocity( speed );
+        RoFI::wait( 1000, [&]{
+            localRofi.getJoint( 0 ).setVelocity( -speed );
+            RoFI::wait( 1000, [&]{
+                localRofi.getJoint( 0 ).setVelocity( 0 );
+                RoFI::wait( 1000, [&]{
+                    endCyclePromise.set_value();
+                } );
+            } );
+        } );
+
+        endCycleFuture.get();
+    }
+
+    std::cout << "Basic force example ended\n";
+}
