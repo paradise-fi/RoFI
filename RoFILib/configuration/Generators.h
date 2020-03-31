e74c6358db85c15b80200ae6bd044b49c6b1d9c8
matej.zacek <matej.zacek@kiwi.com>
2020-03-31 11:50:58 +0200

    RoFILib: Changes after code review of refactoring

--- RoFILib/configuration/Generators.h
+++ RoFILib/configuration/Generators.h
@@ -6,75 +6,88 @@
 #ifndef ROBOTS_GENERATORS_H
 #define ROBOTS_GENERATORS_H
 
-    std::optional<Configuration> executeIfValid(const Configuration& config, const Action &action);
+std::optional<Configuration> executeIfValid(const Configuration& config, const Action &action);
 
-    void generateActions(const Configuration& config, std::vector<Action>& res, unsigned step, unsigned bound=1);
+void generateActions(const Configuration& config, std::vector<Action>& res, unsigned step, unsigned bound=1);
 
-    void generateSimpleActions(const Configuration& config, std::vector<Action>& res, unsigned step);
+void generateSimpleActions(const Configuration& config, std::vector<Action>& res, unsigned step);
 
-    void generateParalyzedActions(const Configuration& config, std::vector<Action>& res, unsigned step, 
-        const std::unordered_set<unsigned>& allowed_indices);
+void generateParalyzedActions(const Configuration& config, std::vector<Action>& res, unsigned step, 
+    const std::unordered_set<ID>& allowed_indices);
 
-    void generateConnections(const Configuration& config, std::vector<Action::Reconnect>& res);
+void generateRotations(const Configuration& config, std::vector<Action::Rotate>& res, unsigned step);
 
-    void generateRotations(const Configuration& config, std::vector<Action::Rotate>& res, unsigned step);
+void generateParalyzedRotations(const Configuration& config, std::vector<Action::Rotate>& res, unsigned step, 
+    const std::unordered_set<ID>& allowed_indices);
 
-    void generateParalyzedRotations(const Configuration& config, std::vector<Action::Rotate>& res, unsigned step, 
-        const std::unordered_set<unsigned>& allowed_indices);
+void generateConnections(const Configuration& config, std::vector<Action::Reconnect>& res);
 
-    void generateDisconnections(const Configuration& config, std::vector<Action::Reconnect>& res);
+void generateParalyzedConnections(const Configuration& config, std::vector<Action::Reconnect>& res,
+    const std::unordered_set<ID>& allowed_indices);
 
-    inline void generateReconnect(const Configuration& config, std::vector<Action::Reconnect>& res) {
-        generateConnections(config, res);
-        generateDisconnections(config, res);
+void generateDisconnections(const Configuration& config, std::vector<Action::Reconnect>& res);
+
+void generateParalyzedDisconnections(const Configuration& config, std::vector<Action::Reconnect>& res,
+    const std::unordered_set<ID>& allowed_indices);
+
+inline void generateReconnect(const Configuration& config, std::vector<Action::Reconnect>& res) {
+    generateConnections(config, res);
+    generateDisconnections(config, res);
+}
+
+inline void generateParalyzedReconnect(const Configuration& config, std::vector<Action::Reconnect>& res,
+    const std::unordered_set<ID>& allowed_indices) 
+{
+    generateParalyzedConnections(config, res, allowed_indices);
+    generateParalyzedDisconnections(config, res, allowed_indices);
+}
+
+void next(const Configuration& config, std::vector<Configuration>& res, unsigned step, unsigned bound=1);
+
+void simpleNext(const Configuration& config, std::vector<Configuration>& res, unsigned step);
+
+void paralyzedNext(const Configuration& config, std::vector<Configuration>& res, unsigned step, 
+    const std::unordered_set<unsigned>& allowed_indices);
+
+template<typename T>
+inline void getAllSubsetsRec(const std::vector<T>& set, std::vector<std::vector<T>>& res, 
+    const std::vector<T>& accum, unsigned index, unsigned count) 
+{
+    if ((accum.size() == count) || (index >= set.size())) {
+        res.push_back(accum);
+        return;
+    }
+    auto next = accum;
+    next.push_back(set[index]);
+    getAllSubsetsRec(set, res, next, index + 1, count);
+    getAllSubsetsRec(set, res, accum, index + 1, count);
+}
+
+using Rots = std::vector<Action::Rotate>;
+template<>
+inline void getAllSubsetsRec(const Rots& set, std::vector<Rots>& res, const Rots& accum, unsigned index, unsigned count) {
+    if ((accum.size() == count) || (index >= set.size())) {
+        res.push_back(accum);
+        return;
     }
 
-    void next(const Configuration& config, std::vector<Configuration>& res, unsigned step, unsigned bound=1);
-
-    void simpleNext(const Configuration& config, std::vector<Configuration>& res, unsigned step);
-
-    void paralyzedNext(const Configuration& config, std::vector<Configuration>& res, unsigned step, 
-        const std::unordered_set<unsigned>& allowed_indices);
-
-    template<typename T>
-    inline void getAllSubsetsRec(const std::vector<T>& set, std::vector<std::vector<T>>& res, 
-        const std::vector<T>& accum, unsigned index, unsigned count) 
-    {
-        if ((accum.size() == count) || (index >= set.size())) {
-            res.push_back(accum);
-            return;
-        }
+    auto act = set[index];
+    bool ok = true;
+    for (auto& act2 : accum) {
+        if ((act.id() == act2.id()) && (act.joint() == act2.joint()))
+            ok = false;
+    }
+    if (ok) {
         auto next = accum;
         next.push_back(set[index]);
         getAllSubsetsRec(set, res, next, index + 1, count);
-        getAllSubsetsRec(set, res, accum, index + 1, count);
     }
+    getAllSubsetsRec(set, res, accum, index + 1, count);
+}
 
-    using Rots = std::vector<Action::Rotate>;
-    template<>
-    inline void getAllSubsetsRec(const Rots& set, std::vector<Rots>& res, const Rots& accum, unsigned index, unsigned count) {
-        if ((accum.size() == count) || (index >= set.size())) {
-            res.push_back(accum);
-            return;
-        }
-
-        auto act = set[index];
-        bool ok = true;
-        for (auto& act2 : accum) {
-            if ((act.id() == act2.id()) && (act.joint() == act2.joint()))
-                ok = false;
-        }
-        if (ok) {
-            auto next = accum;
-            next.push_back(set[index]);
-            getAllSubsetsRec(set, res, next, index + 1, count);
-        }
-        getAllSubsetsRec(set, res, accum, index + 1, count);
-    }
-
-    template<typename T>
-    void getAllSubsets(const std::vector<T>& from, std::vector<std::vector<T>>& res, unsigned count) {
-        getAllSubsetsRec<T>(from, res, {}, 0, count);
-    }
+template<typename T>
+void getAllSubsets(const std::vector<T>& from, std::vector<std::vector<T>>& res, unsigned count) {
+    getAllSubsetsRec<T>(from, res, {}, 0, count);
+}
 
  #endif //ROBOTS_GENERATORS_H
--- RoFILib/snake_reconfig/Snake_algorithms.h
+++ RoFILib/snake_reconfig/Snake_algorithms.h
@@ -3,6 +3,7 @@
 #include <Configuration.h>
 #include <IO.h>
 #include <Generators.h>
+#include <Algorithms.h>
 #include "MinMaxHeap.h"
 #include "Snake_structs.h"
 #include <limits>
@@ -11,89 +12,12 @@
 #include <memory>
 #include <stack>
 
-struct AlgorithmStat
-{
-    unsigned long pathLength = 0;
-    unsigned long queueSize = 0;
-    unsigned long seenCfgs = 0;
 
-    std::string toString() const
-    {
-        std::stringstream out;
-        out << std::setw(8) << std::left << "length " << pathLength << std::endl;
-        out << std::setw(8) << std::left << "queue " << queueSize << std::endl;
-        out << std::setw(8) << std::left << "cfgs " << seenCfgs << std::endl;
-        return out.str();
-    }
-};
-
-class ConfigurationPtrHash
-{
+struct SnakeEvalCompare {
 public:
-    std::size_t operator()(const std::unique_ptr<Configuration>& ptr) const
-    {
-        return ConfigurationHash{}(*ptr);
-    }
-};
-
-inline bool operator==(const std::unique_ptr<Configuration>& a, const std::unique_ptr<Configuration>& b)
-{
-    return (*a) == (*b);
-}
-
-using ConfigPred = std::unordered_map<const Configuration*, const Configuration*>;
-using ConfigEdges = std::unordered_map<const Configuration*, std::vector<const Configuration*>>;
-using ConfigValue = std::unordered_map<const Configuration*, double>;
-using EvalFunction = double(const Configuration&, const Configuration&);
-using DistFunction = double(const Configuration&, const Configuration&);
-using EvalPair = std::tuple<double, const Configuration*>;
-
-struct EvalCompare
-{
-public:
-    bool operator()(const EvalPair& a, const EvalPair& b) const
-    {
+    bool operator()(const EvalPair& a, const EvalPair& b) const {
         return std::get<0>(a) < std::get<0>(b);
     }
-};
-
-class ConfigPool
-{
-public:
-    typedef std::unordered_set<std::unique_ptr<Configuration>, ConfigurationPtrHash>::iterator iterator;
-    typedef std::unordered_set<std::unique_ptr<Configuration>, ConfigurationPtrHash>::const_iterator const_iterator;
-
-    iterator begin() { return pool.begin(); }
-    const_iterator begin() const { return pool.begin(); }
-    iterator end() { return pool.end(); }
-    const_iterator end() const { return pool.end(); }
-
-    size_t size() const
-    {
-        return pool.size();
-    }
-
-    Configuration* insert(const Configuration& config)
-    {
-        auto tmp = std::make_unique<Configuration>(config);
-        auto [ptr, _] = pool.insert(std::move(tmp));
-        return ptr->get();
-    }
-
-    bool find(const Configuration& config) const
-    {
-        auto tmp = std::make_unique<Configuration>(config);
-        return (pool.find(tmp) != pool.end());
-    }
-
-    const Configuration* get(const Configuration& config) const
-    {
-        auto tmp = std::make_unique<Configuration>(config);
-        return pool.find(tmp)->get();
-    }
-
-private:
-    std::unordered_set<std::unique_ptr<Configuration>, ConfigurationPtrHash> pool;
 };
 
 inline double eval(const Configuration& configuration, SpaceGrid& sg) {
@@ -101,65 +25,8 @@ inline double eval(const Configuration& configuration, SpaceGrid& sg) {
     return sg.getDist();
 }
 
-inline std::vector<Configuration> createPath(ConfigPred& pred, const Configuration* goal)
-{
-    std::vector<Configuration> res;
-    const Configuration* current = goal;
-
-    while (current != pred.at(current))
-    {
-        res.push_back(*current);
-        current = pred.at(current);
-    }
-    res.push_back(*current);
-
-    std::reverse(res.begin(), res.end());
-    return res;
-}
-
-inline std::vector<Configuration> createPath(ConfigEdges& edges, const Configuration* init, const Configuration* goal)
-{
-    ConfigPred pred;
-    std::unordered_set<const Configuration*> seen;
-    if (init == goal)
-    {
-        return {*init};
-    }
-
-    seen.insert(init);
-    pred.insert({init, init});
-
-    std::queue<const Configuration*> queue;
-    queue.push(init);
-
-    while (!queue.empty())
-    {
-
-        const auto curr = queue.front();
-        queue.pop();
-
-        for (const auto* next : edges[curr])
-        {
-            if (seen.find(next) == seen.end())
-            {
-                seen.insert(next);
-                pred.insert({next, curr});
-
-                if (next == goal)
-                {
-                    return createPath(pred, goal);
-                }
-
-                queue.push(next);
-            }
-        }
-    }
-
-    return {};
-}
-
-
-inline std::vector<Configuration> SnakeStar(const Configuration& init, AlgorithmStat* stat = nullptr, int limit = 5000, double path_pref = 0.5)
+inline std::vector<Configuration> SnakeStar(const Configuration& init, AlgorithmStat* stat = nullptr, 
+    int limit = 5000, double path_pref = 0.1)
 {
     unsigned step = 90;
     double free_pref = 1 - path_pref;
@@ -176,11 +43,9 @@ inline std::vector<Configuration> SnakeStar(const Configuration& init, Algorithm
     double startDist = eval(init, grid);
 
     if (startDist == 0)
-    {
         return {init};
-    }
 
-    MinMaxHeap<EvalPair, EvalCompare> queue(limit);
+    MinMaxHeap<EvalPair, SnakeEvalCompare> queue(limit);
 
     const Configuration* pointer = pool.insert(init);
     const Configuration* bestConfig = pointer;
@@ -195,9 +60,7 @@ inline std::vector<Configuration> SnakeStar(const Configuration& init, Algorithm
     queue.push( {goalDist[pointer], pointer} );
 
 
-    while (!queue.empty() && i++ < limit)
-    {
-
+    while (!queue.empty() && i++ < limit) {
         maxQSize = std::max(maxQSize, queue.size());
         const auto [d, current] = queue.popMin();
         double currDist = initDist[current];
@@ -205,34 +68,29 @@ inline std::vector<Configuration> SnakeStar(const Configuration& init, Algorithm
         std::vector<Configuration> nextCfgs;
         simpleNext(*current, nextCfgs, step);
 
-        for (const auto& next : nextCfgs)
-        {
+        for (const auto& next : nextCfgs) {
             const Configuration* pointerNext;
             double newEval = eval(next, grid);
             double newDist = path_pref * (currDist + 1) + free_pref * newEval;
             bool update = false;
 
             if (newEval != 0 && limit <= queue.size() + i) {
-                if (newDist > worstDist) {
+                if (newDist > worstDist)
                     continue;
-                }
+
                 worstDist = newDist;
-                if (!queue.empty()) {
+                if (!queue.empty())
                     queue.popMax();
-                }
-            }
-            if (newDist > worstDist) {
-                worstDist = newDist;
             }
 
-            if (!pool.find(next))
-            {
+            if (newDist > worstDist)
+                worstDist = newDist;
+
+            if (!pool.has(next)) {
                 pointerNext = pool.insert(next);
                 initDist[pointerNext] = currDist + 1;
                 update = true;
-            }
-            else
-            {
+            } else {
                 pointerNext = pool.get(next);
             }
 
@@ -241,32 +99,26 @@ inline std::vector<Configuration> SnakeStar(const Configuration& init, Algorithm
                 bestConfig = pointerNext;
             }
 
-            if ((currDist + 1 < initDist[pointerNext]) || update)
-            {
+            if ((currDist + 1 < initDist[pointerNext]) || update) {
                 initDist[pointerNext] = currDist + 1;
                 goalDist[pointerNext] = newDist;
                 pred[pointerNext] = current;
                 queue.push({newDist, pointerNext});
             }
 
-            if (newEval == 0)
-            {
+            if (newEval == 0) {
                 auto path = createPath(pred, pointerNext);
-                if (stat != nullptr)
-                {
+                if (stat != nullptr) {
                     stat->pathLength = path.size();
                     stat->queueSize = maxQSize;
                     stat->seenCfgs = pool.size();
                 }
-
                 return path;
             }
         }
     }
-    std::cout << "Just returning my best!" << std::endl;
     auto path = createPath(pred, bestConfig);
-    if (stat != nullptr)
-    {
+    if (stat != nullptr) {
         stat->pathLength = path.size();
         stat->queueSize = maxQSize;
         stat->seenCfgs = pool.size();
@@ -291,8 +143,7 @@ ID closestMass(const Configuration& init) {
     return bestID;
 }
 
-class MakeStar
-{
+class MakeStar {
 public:
     MakeStar(const Configuration& init, ID root)
     : mass(init.massCenter())
@@ -300,9 +151,9 @@ public:
     , dists() {
         for (const auto& [id, ms] : init.getMatrices()) {
             double currDist = 0;
-            for (unsigned side = 0; side < 2; ++side) {
+            for (unsigned side = 0; side < 2; ++side)
                 currDist += sqDistVM(ms[side], mass);
-            }
+
             dists.emplace(id, currDist);
         }
     }
@@ -312,9 +163,9 @@ public:
         std::sort(nEdges.begin(), nEdges.end(), [&](const Edge& a, const Edge& b){
             return dists[a.id2()] < dists[b.id2()];
         });
-        for (const auto& e : nEdges) {
+        for (const auto& e : nEdges)
             dfs_stack.push(e.id2());
-        }
+
         return nEdges;
     }
 
@@ -328,8 +179,7 @@ private:
 using chooseRootFunc = ID(const Configuration&);
 
 template<typename Next>
-inline Configuration treefy(const Configuration& init, chooseRootFunc chooseRoot = closestMass)
-{
+inline Configuration treefy(const Configuration& init, chooseRootFunc chooseRoot = closestMass) {
     ID root = chooseRoot(init);
     Configuration treed = init;
     treed.clearEdges();
@@ -341,103 +191,23 @@ inline Configuration treefy(const Configuration& init, chooseRootFunc chooseRoot
 
     dfs_stack.push(root);
 
-    int counter = 0;
-
     while(!dfs_stack.empty()) {
         ID curr = dfs_stack.top();
         dfs_stack.pop();
-        if (seen.find(curr) != seen.end()) {
+        if (seen.find(curr) != seen.end())
             continue;
-        }
+
         seen.insert(curr);
         std::vector<Edge> edges = oracle(dfs_stack, seen, curr);
-        for (const auto& e : edges) {
+        for (const auto& e : edges)
             treed.addEdge(e);
-        }
     }
 
     return treed;
 }
 
-namespace Eval
-{
-    inline double trivial(const Configuration& conf, const Configuration& goal)
-    {
-        return 1;
-    }
-
-    inline double jointDiff(const Configuration& curr, const Configuration& goal)
-    {
-        double result = 0;
-        for ( auto& [id, mod] : curr.getModules() )
-        {
-            const Module& other = goal.getModules().at(id);
-            for (Joint j : {Alpha, Beta, Gamma})
-            {
-                double diff = mod.getJoint(j) - other.getJoint(j);
-                //result += sqrt(diff * diff);
-                result += std::abs(diff) / 90;
-            }
-        }
-        return result;
-    }
-
-    inline double centerDiff(const Configuration& curr, const Configuration& goal)
-    {
-        double result = 0;
-        for ( auto& [id, ms] : curr.getMatrices() )
-        {
-            const auto& other = goal.getMatrices().at(id);
-            for (ShoeId s : {ShoeId::A, ShoeId::B})
-            {
-                result += distance(center(ms[s]), center(other[s]));
-            }
-        }
-        return result;
-    }
-
-    inline double matrixDiff(const Configuration& curr, const Configuration& goal)
-    {
-        double result = 0;
-        for ( auto& [id, ms] : curr.getMatrices() )
-        {
-            const auto& other = goal.getMatrices().at(id);
-            for (ShoeId s : {ShoeId::A, ShoeId::B})
-            {
-                result += distance(ms[s], other[s]);
-            }
-        }
-        return result;
-    }
-
-    inline double actionDiff(const Configuration& curr, const Configuration& goal)
-    {
-        Action action = curr.diff(goal);
-        return action.rotations().size() + action.reconnections().size() ;
-    }
-}
-
-namespace Distance
-{
-    inline double reconnections(const Configuration& curr, const Configuration& goal)
-    {
-        auto diff = curr.diff(goal);
-        return diff.reconnections().size();
-    }
-
-    inline double rotations(const Configuration& curr, const Configuration& goal)
-    {
-        auto diff = curr.diff(goal);
-        return diff.rotations().size();
-    }
-
-    inline double diff(const Configuration& curr, const Configuration& goal)
-    {
-        return reconnections(curr, goal) * 1000 + rotations(curr, goal);
-    }
-}
-
-inline std::vector<Configuration> paralyzedAStar(const Configuration& init, const Configuration& goal, EvalFunction& eval = Eval::trivial, AlgorithmStat* stat = nullptr, std::unordered_set<unsigned> allowed_indices = {})
+inline std::vector<Configuration> paralyzedAStar(const Configuration& init, const Configuration& goal, 
+    EvalFunction& eval = Eval::trivial, AlgorithmStat* stat = nullptr, std::unordered_set<unsigned> allowed_indices = {})
 {
     ConfigPred pred;
     ConfigPool pool;
@@ -450,11 +220,9 @@ inline std::vector<Configuration> paralyzedAStar(const Configuration& init, cons
     ConfigValue goalDist;
 
     if (init == goal)
-    {
         return {init};
-    }
 
-    std::priority_queue<EvalPair, std::vector<EvalPair>, EvalCompare> queue;
+    std::priority_queue<EvalPair, std::vector<EvalPair>, SnakeEvalCompare> queue;
 
     const Configuration* pointer = pool.insert(init);
     initDist[pointer] = 0;
@@ -464,58 +232,46 @@ inline std::vector<Configuration> paralyzedAStar(const Configuration& init, cons
 
     queue.push( {goalDist[pointer], pointer} );
 
-    while (!queue.empty())
-    {
-
+    while (!queue.empty()) {
         maxQSize = std::max(maxQSize, queue.size());
         const auto [d, current] = queue.top();
         double currDist = initDist[current];
         queue.pop();
 
-
         std::vector<Configuration> nextCfgs;
         paralyzedNext(*current, nextCfgs, step, allowed_indices);
-        for (const auto& next : nextCfgs)
-        {
+        for (const auto& next : nextCfgs) {
             const Configuration* pointerNext;
             double newDist = currDist + 1 + eval(next, goal);
             bool update = false;
 
-            if (!pool.find(next))
-            {
+            if (!pool.has(next)) {
                 pointerNext = pool.insert(next);
                 initDist[pointerNext] = currDist + 1;
                 update = true;
-            }
-            else
-            {
+            } else {
                 pointerNext = pool.get(next);
             }
 
-            if ((currDist + 1 < initDist[pointerNext]) || update)
-            {
+            if ((currDist + 1 < initDist[pointerNext]) || update) {
                 initDist[pointerNext] = currDist + 1;
                 goalDist[pointerNext] = newDist;
                 pred[pointerNext] = current;
                 queue.push({newDist, pointerNext});
             }
 
-            if (next == goal)
-            {
+            if (next == goal) {
                 auto path = createPath(pred, pointerNext);
-                if (stat != nullptr)
-                {
+                if (stat != nullptr) {
                     stat->pathLength = path.size();
                     stat->queueSize = maxQSize;
                     stat->seenCfgs = pool.size();
                 }
-
                 return path;
             }
         }
     }
-    if (stat != nullptr)
-    {
+    if (stat != nullptr) {
         stat->pathLength = 0;
         stat->queueSize = maxQSize;
         stat->seenCfgs = pool.size();
