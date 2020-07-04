264ec531e782409af3df9b765682bdc5d76af17d
Vladimír Chlup <jajik@chlup.net>
2020-07-04 11:58:54 +0200

    RoFIDriver: Reimplement RoFI network Interface (RoIF).
    Separate physical (Connector's) netif from the RoIF itself. Drop "6" from class names.

---
+++ RoFIDriver/networking/include/physical_netif.hpp
@@ -0,0 +1,164 @@
+#pragma once
+
+#include "rofi_hal.hpp"
+#include "routing_table.hpp"
+
+#include <lwip/mld6.h>
+#include <lwip/raw.h>
+#include <lwip/ip6_addr.h>
+
+#include <algorithm>
+#include <string>
+#include <cassert>
+#include <sstream>
+
+#include <iostream>
+
+namespace rofinet {
+	using namespace rofi::hal;
+
+static int seqNum() {
+    static int num = 0;
+    return num++;
+}
+
+struct PhysAddr {
+	PhysAddr( uint8_t *a ) {
+		std::copy_n( a, 6, addr );
+	}
+	PhysAddr( uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f ) {
+        addr[ 0 ] = a; addr[ 1 ] = b; addr[ 2 ] = c;
+        addr[ 3 ] = d; addr[ 4 ] = e; addr[ 5 ] = f;
+    }
+
+	PhysAddr( const PhysAddr& ) = default;
+    PhysAddr& operator=( const PhysAddr& ) = default;
+
+	static int size() { return 6; }
+
+    uint8_t addr[ 6 ];
+};
+
+
+class PhysNetif {
+public:
+	PhysNetif( PhysAddr pAddr, Connector connector, RTable& rt )
+	: pAddr( pAddr ), connector( connector ), rtable( rt ) {
+		connector.onPacket( [ this ]( Connector c, uint16_t contentType, PBuf&& pb ) {
+			onPacket( c, contentType, std::move( pb ) );
+		} );
+
+		connector.onConnectorEvent( [ this ]( Connector, ConnectorEvent e) {
+			if ( e == ConnectorEvent::Connected ) {
+				sendRRP();
+			} else {
+				rtable.removeForIf( &netif );
+			}
+		} );
+
+        netif_add( &netif, nullptr, nullptr, nullptr, this, init, tcpip_input );
+        setRRP();
+	}
+
+	bool isConnected() {
+		return connector.getState().connected;
+	}
+
+	void setUp() {
+		netif_create_ip6_linklocal_address( &netif, 0 );
+		netif_set_up( &netif );
+		if ( isConnected() )
+			sendRRP();
+	}
+
+	static err_t init( struct netif* n ) {
+		auto self = reinterpret_cast< PhysNetif* >( n->state );
+		n->hwaddr_len = 6;
+		std::copy_n( self->pAddr.addr, 6, n->hwaddr );
+		n->mtu = 120;
+		n->name[ 0 ] = 'r'; n->name[ 1 ] = 'd';
+		n->num = seqNum();
+		n->output_ip6 = output;
+		n->input = netif_input;
+		n->linkoutput = linkOutput;
+		n->flags |= NETIF_FLAG_IGMP | NETIF_FLAG_MLD6;
+		return ERR_OK;
+	}
+
+	static err_t output( struct netif* n, struct pbuf *p, const ip6_addr_t* ip ) {
+		auto self = reinterpret_cast< PhysNetif* >( n->state );
+		self->send( Ip6Addr( *ip ), PBuf::reference( p ) );
+
+		return ERR_OK;
+	}
+
+	static err_t linkOutput( struct netif*, struct pbuf* ) {
+		assert( false && "Link output should not be directly used" );
+		return ERR_OK;
+	}
+
+	bool sendRRP( RTable::Command cmd = RTable::Command::Call ) {
+		ip_addr_t ip;
+	    ipaddr_aton( "ff02::1f", &ip );
+		auto rrp = rtable.createRRPmsgIfless( &netif, cmd );
+		err_t res = raw_sendto_if_src( pcb, rrp.release(), &ip, &netif, &netif.ip6_addr[ 0 ] ); // use link-local address as source
+		return res == ERR_OK;
+	}
+
+	bool isStub() const {
+		return stub;
+	}
+
+	bool setStub( bool b ) {
+		return stub = b;
+	}
+	
+	void send( const Ip6Addr&, PBuf&& packet, int contentType = 0 ) {
+		connector.send( contentType, std::move( packet) );
+	}
+
+private:
+	void onPacket( Connector c, uint16_t contentType, PBuf&& packet ) {
+		if ( contentType == 0 ) {
+			netif.input( packet.release(), &netif );
+		}
+	}
+
+	u8_t static onRRP( void *arg, struct raw_pcb*, struct pbuf *p, const ip_addr_t* ) {
+		auto self = reinterpret_cast< PhysNetif* >( arg );
+		if ( self ) {
+			p = pbuf_free_header( p, IP6_HLEN );
+			if ( self->rtable.update( PBuf::reference( p ), &self->netif ) )
+				self->sendRRP( self->rtable.isStub() ? RTable::Command::Stubby : RTable::Command::Response );
+			return 1;
+		}
+		return 0;
+	}
+
+	void setRRP() {
+        create_rrp_listener();
+		ip6_addr_t ip;
+		ip6addr_aton( "ff02::1f", &ip );
+		auto res = mld6_joingroup_netif( &netif, &ip );
+		if ( res != ERR_OK )
+			std::cout << "Error - failed to join mld6 group: " << lwip_strerr( res ) << std::endl;
+	}
+
+	void create_rrp_listener() {
+		pcb = raw_new_ip6( IP6_NEXTH_ICMP6 );
+		if ( pcb == nullptr ) 
+			assert( false && "raw_new_ip6 failed, pcb is null!");
+		raw_bind_netif( pcb, &netif );
+		raw_recv( pcb, onRRP, this );
+	}
+
+	Netif netif;
+	struct raw_pcb* pcb = nullptr;
+	PhysAddr pAddr;
+	Connector connector;
+	bool stub = false;
+	RTable& rtable;
+};
+	
+
+} // namespace rofinet
---
+++ RoFIDriver/networking/include/roif.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "physical_netif.hpp"
+
+#include <cassert>
+#include <vector>
+#include <sstream>
+
+namespace rofinet {
+	using namespace rofi::hal;
+
+class RoIF : public Netif {
+public:
+	RoIF( RoFI rofi ) : RoIF( rofi, createAddress( rofi.getId() ), 128 ) {}
+	RoIF( RoFI rofi, const Ip6Addr& ip, uint8_t mask ) {
+		int id = rofi.getId();
+		netif_add( &netif, nullptr, nullptr, nullptr, this, init, tcpip_input );
+		addAddress( ip, mask );
+		netif_set_default( this );
+
+		int connectors = rofi.getDescriptor().connectorCount;
+		PhysAddr pAddr( id, id, id, id, id, id );
+
+		netifs.reserve( connectors );
+		for ( int i = 0; i < connectors; i++ ) {
+			netifs.emplace_back( pAddr, rofi.getConnector( i ), rtable );
+		}
+	}
+
+	bool addAddress( const Ip6Addr& ip, uint8_t mask ) {
+		s8_t index = -1;
+		netif_add_ip6_address( &netif, &ip, &index );
+		return index >= 0 && rtable.add( ip, mask, 0, this );
+	}
+
+	bool removeAddress( const Ip6Addr& ip, uint8_t mask ) {
+		int index = 0;
+		for ( ; index < LWIP_IPV6_NUM_ADDRESSES; index++ ) {
+			if ( ip == Ip6Addr( *ip_2_ip6( &ip6_addr[ index ] ) ) ) {
+				ip_addr_set_zero( &ip6_addr[ index ] );
+				return rtable.remove( ip, mask, this );
+			}
+		}
+
+		return false;
+	}
+
+	void setUp() {
+        netif_set_up( this );
+		rtable.setBroadcast( [ this ]( RTable::Command cmd ) { broadcastRTable( cmd ); } );
+		for ( auto& n : netifs ) {
+			n.setUp();
+		}
+	}
+
+	void send( const Ip6Addr& ip, PBuf&& packet, int contentType = 0 ) {
+		auto n = ip_find_route( &ip );
+		if ( n )
+			n->output_ip6( n, packet.release(), &ip );
+	}
+
+	static err_t init( struct netif* roif ) {
+        roif->mtu = 120;
+        roif->name[ 0 ] = 'r'; roif->name[ 1 ] = 'l';
+        roif->num = seqNum();
+        roif->output_ip6 = output;
+        roif->linkoutput = linkOutput;
+        roif->flags |= NETIF_FLAG_LINK_UP | NETIF_FLAG_IGMP | NETIF_FLAG_UP | NETIF_FLAG_MLD6;
+        return ERR_OK;
+    }
+
+    static err_t output( struct netif* roif, struct pbuf *p, const ip6_addr_t *ipaddr ) {
+        auto self = reinterpret_cast< RoIF* >( roif );
+        self->send( Ip6Addr( *ipaddr ), PBuf::reference( p ) );
+        return ERR_OK;
+    }
+
+    static err_t linkOutput( struct netif*, struct pbuf* ) {
+        assert( false && "Link output should not be directly used" );
+        return ERR_OK;
+    }
+
+	void broadcastRTable( RTable::Command cmd = RTable::Command::Call ) {
+		for ( auto& n : netifs ) {
+			if ( n.isConnected() )
+				n.sendRRP( cmd );
+		}
+	}
+
+	const RTable& getRTable() const {
+		return rtable;
+	}
+
+private:
+	Ip6Addr createAddress ( int id ) {
+		std::ostringstream s;
+		s << "fc07::" << id;
+		return Ip6Addr( s.str().c_str() );
+	}
+
+	Netif netif;
+	RTable rtable;
+	std::vector< PhysNetif > netifs;
+};
+
+} // namespace rofinet
