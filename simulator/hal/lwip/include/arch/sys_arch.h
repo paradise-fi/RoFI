7482a0d5397ab98a24d9c29c4c57e76ca77d3424
Vladimír Chlup <jajik@chlup.net>
2020-07-04 11:45:13 +0200

    RoFISim: Move core locking definitions for the lwip to sys_arch.h.

--- simulator/hal/lwip/include/arch/sys_arch.h
+++ simulator/hal/lwip/include/arch/sys_arch.h
@@ -59,4 +59,21 @@ typedef struct sys_mbox * sys_mbox_t;
 struct sys_thread;
 typedef struct sys_thread * sys_thread_t;
 
+
+/*
+   ---------------------------------------
+   ---------- Threading options ----------
+   ---------------------------------------
+*/
+
+void sys_mark_tcpip_thread(void);
+#define LWIP_MARK_TCPIP_THREAD()   sys_mark_tcpip_thread()
+
+#if LWIP_TCPIP_CORE_LOCKING
+void sys_lock_tcpip_core(void);
+#define LOCK_TCPIP_CORE()          sys_lock_tcpip_core()
+void sys_unlock_tcpip_core(void);
+#define UNLOCK_TCPIP_CORE()        sys_unlock_tcpip_core()
+#endif
+
 #endif /* LWIP_ARCH_SYS_ARCH_H */
--- simulator/hal/lwip/include/lwipopts.h
+++ simulator/hal/lwip/include/lwipopts.h
@@ -442,20 +442,13 @@
    ---------------------------------------
 */
 
-// #define LWIP_TCPIP_CORE_LOCKING    0
+#define LWIP_TCPIP_CORE_LOCKING         0
 
 // #if !NO_SYS
 // void sys_check_core_locking(void);
 // #define LWIP_ASSERT_CORE_LOCKED()  sys_check_core_locking()
 // void sys_mark_tcpip_thread(void);
 // #define LWIP_MARK_TCPIP_THREAD()   sys_mark_tcpip_thread()
-
-// #if LWIP_TCPIP_CORE_LOCKING
-// void sys_lock_tcpip_core(void);
-// #define LOCK_TCPIP_CORE()          sys_lock_tcpip_core()
-// void sys_unlock_tcpip_core(void);
-// #define UNLOCK_TCPIP_CORE()        sys_unlock_tcpip_core()
-// #endif
 // #endif
 
 #endif /* LWIP_LWIPOPTS_H */
