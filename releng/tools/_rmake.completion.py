9189c77b77af5b783eccfac70ad074fdec396315
Jan Mrázek <email@honzamrazek.cz>
2021-04-04 16:38:10 +0200

    releng: Improve completion for rmake --test

--- releng/tools/_rmake.completion.py
+++ releng/tools/_rmake.completion.py
@@ -9,30 +9,42 @@ flags = [
     "--list",
     "--doc",
     "--help",
+    "--test"
     ]
 
 word = int(sys.argv[1])
 words = sys.argv[3:]
+test = False
 
 if len(words) > 0 and words[0].startswith("--"):
     if word == 1:
         candidates = [ s for s in flags if s.startswith(words[0]) ]
         print("\n".join(candidates))
-    sys.exit(0)
+    if words[0] != "--test":
+        sys.exit(0)
+    else:
+        test = True
+        if "--all" not in words:
+            print("--all")
 
 candidates = []
 if word == 1:
     candidates += flags
 
-targets = availableTargets()
+
+if test:
+    targets = availableTestTargets()
+else:
+    targets = availableTargets()
+
 candidates += [t.target for t in targets]
 candidates += [t.path for t in targets]
 
 suites = availableSuites()
 candidates += suites
-candidates += ["clean-" + s for s in suites]
-
-candidates += ["clean"]
+if not test:
+    candidates += ["clean-" + s for s in suites]
+    candidates += ["clean"]
 
 
 if word >= 1 and word - 1 < len(words):
