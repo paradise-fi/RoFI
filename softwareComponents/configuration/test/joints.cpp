6c9a275e3f2ca9f4435ad32cf2c38fdcfad2f7dc
Vladimír Chlup <jajik@chlup.net>
2021-05-21 11:48:12 +0200

    configuration: Add more tests
    Improve formatting and fix bugs

--- softwareComponents/configuration/test/joints.cpp
+++ softwareComponents/configuration/test/joints.cpp
@@ -3,56 +3,95 @@
 #include <joints.h>
 #include <iostream>
 
-void printMatrix( Matrix m ) {
-    for ( int i = 0; i < m.n_rows; i++ ) {
-        for ( int j = 0; j < m.n_cols; j++ ) {
-            std::cout << round( m(i, j) ) << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-}
-
 TEST_CASE( "Base RigidJoint" ) {
     // ToDo: Write the test
-    auto j = rofi::RigidJoint( rotate( M_PI_2, { 1, 0, 0, 1 } ) * translate( { 20, 0, 0 } ) );
+    auto j = rofi::RigidJoint( translate( { 42, 42, 42 } ) );
+    CHECK( equals( j.sourceToDest(), translate( { 42, 42, 42 } ) ) );
+    CHECK( equals( j.destToSource(), translate( { -42, -42, -42 } ) ) );
+
+    j = rofi::RigidJoint( rotate( M_PI_2, { 1, 0, 0 } ) * translate( { 20, 0, 0 } ) );
     CHECK( equals( j.sourceToDest(), { { 1, 0,  0, 20 }
                                      , { 0, 0, -1,  0 }
                                      , { 0, 1,  0,  0 }
                                      , { 0, 0,  0,  1 } } ) );
     CHECK( equals( j.sourceToDest() * j.destToSource(), identity ) );
+    CHECK( equals( j.sourceToDest(), rotate( M_PI_2, { 1, 0, 0 } ) * translate( { 20, 0, 0 } ) ) );
 }
 
 TEST_CASE( "Base RotationJoint" ) {
-    // ToDo: Write the test
-    // Vector sourceOrigin, Vector sourceAxis, Vector destOrigin, Vector desAxis, double min, double max
-    auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 1, 0, 0 }, { 1, 0, 0 }, -M_PI_2, M_PI_2 );
-    std::cout << "joint created is " << j << "\n";
-    j.positions = { 0 * M_PI }; // set the angle of the rotation
-    CHECK( equals( j.sourceToDest(), identity ) );
-    CHECK( equals( j.destToSource(), identity ) );
+    SECTION( "basic creation at one point" ) {
+        // Vector sourceOrigin, Vector sourceAxis, Vector destOrigin, Vector desAxis, double min, double max
+        auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }, -M_PI_2, M_PI_2 );
+        j.positions = { 0 * M_PI }; // set the angle of the rotation
+        CHECK( equals( j.sourceToDest(), identity ) );
+        CHECK( equals( j.destToSource(), identity ) );
+    }
+
+    SECTION( "rotation by 0" ) {
+        auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 42, 42, 42 }, { 1, 0, 0 }, -M_PI, M_PI );
+        j.positions = { 0 };
+        CHECK( equals( j.sourceToDest(), translate( { 42, 42, 42 } ) ) );
+        CHECK( equals( j.destToSource(), translate( { -42, -42, -42 } ) ) );
+        CHECK( equals( j.sourceToDest() * j.destToSource(), identity ) );
+        j = rofi::RotationJoint( identity, { 1, 0, 0 }, translate( { 42, 42, 42 } ), -M_PI_2, M_PI_2 );
+        j.positions = { 0 };
+        CHECK( equals( j.sourceToDest(), translate( { 42, 42, 42 } ) ) );
+        CHECK( equals( j.destToSource(), translate( { -42, -42, -42 } ) ) );
+        CHECK( equals( j.sourceToDest() * j.destToSource(), identity ) );
+    }
+
+    SECTION( "sourceToDest on a ptr of type Joint" ) {
+        auto j = rofi::RotationJoint( identity, { 1, 0, 0 }, translate( { 42, 42, 42 } ), -M_PI_2, M_PI_2 );
+        REQUIRE( j.positions.size() == 0 );
+        auto* jj = static_cast< rofi::Joint* >( &j );
+        jj->sourceToDest( { M_PI } );
+        REQUIRE( j.positions.size() == 1 );
+        CHECK( j.positions[ 0 ] == M_PI );
+    }
 }
 
 TEST_CASE( "sourceToDest and destToSource" ) {
     SECTION( "Unit size" ) {
         auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 1, 0, 0 }, { 1, 0, 0 }, -M_PI_2, M_PI_2 );
         j.positions = { 0 * M_PI };
-        CHECK( equals( j.destToSource(), j.sourceToDest() ) );
+        CHECK( equals( j.sourceToDest(), translate( {  1, 0, 0 } ) ) );
+        CHECK( equals( j.destToSource(), translate( { -1, 0, 0 } ) ) );
         j.positions = { 2 * M_PI };
-        CHECK( equals( j.destToSource(), j.sourceToDest() ) );
+        CHECK( equals( j.sourceToDest(), translate( { 1, 0, 0 } ) ) );
         j.positions = { M_PI_2 };
-        CHECK( equals( j.destToSource() * j.sourceToDest(), identity ) );
+        CHECK( equals( j.sourceToDest(), rotate( M_PI_2, { 1, 0, 0 } ) * translate( { 1, 0, 0 } ) ) );
+        CHECK( equals( j.sourceToDest() * j.destToSource(), identity ) );
         CHECK_FALSE( equals( j.destToSource(), j.sourceToDest() ) );
     }
 
-    SECTION( "Asymetric" ) {
-        auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 5, 0, 0 }, { 4, 0, 0 }, -M_PI_2, M_PI_2 );
+    SECTION( "Bigger size" ) {
+        auto j = rofi::RotationJoint( { 0, 0, 0 }, { 0, 0, 0 }, { 5, 0, 0 }, { 0, 1, 0 }, -M_PI_2, M_PI_2 );
         j.positions = { 0 * M_PI };
-        CHECK( equals( j.destToSource(), j.sourceToDest() ) );
-        j.positions = { 2 * M_PI };
-        CHECK( equals( j.destToSource(), j.sourceToDest() ) );
+        CHECK( equals( j.sourceToDest(), translate( { 5, 0, 0 } ) ) );
+        j.positions = { M_PI };
+        CHECK( equals( j.sourceToDest(), rotate( M_PI, { 0, 1, 0 } ) * translate( { 5, 0, 0 } ) ) );
+        CHECK( equals( center( j.sourceToDest() ), { -5, 0, 0, 1 } ) );
         j.positions = { M_PI_2 };
-        CHECK( equals( j.destToSource() * j.sourceToDest(), identity ) );
-        CHECK_FALSE( equals( j.destToSource(), j.sourceToDest() ) );
+        CHECK( equals( j.sourceToDest(), rotate( M_PI_2, { 0, 1, 0 } ) * translate( { 5, 0, 0 } ) ) );
+        CHECK( equals( center( j.sourceToDest() ), { 0, 0, -5, 1 } ) );
+        j.positions = { -M_PI_2 };
+        CHECK( equals( j.sourceToDest(), rotate( -M_PI_2, { 0, 1, 0 } ) * translate( { 5, 0, 0 } ) ) );
+        CHECK( equals( center( j.sourceToDest() ), { 0, 0, 5, 1 } ) );
+        CHECK( equals( j.sourceToDest() * j.destToSource(), identity ) );
+    }
+}
+
+TEST_CASE( "joint limits" ) {
+    SECTION( "RigidJoint" ) {
+        auto j = rofi::RigidJoint( identity );
+        CHECK( j.paramCount() == 0 );
+        CHECK_THROWS( j.jointLimits( 10 ) );
+    }
+
+    SECTION( "RotationJoint" ) {
+        auto j = rofi::RotationJoint( identity, { 0, 2, 1 }, identity, - M_PI, M_PI );
+        CHECK( j.paramCount() == 1 );
+        CHECK_THROWS( j.jointLimits( 1 ) );
+        CHECK_NOTHROW( j.jointLimits( 0 ) );
     }
 }
--- softwareComponents/configuration/test/rofibot.cpp
+++ softwareComponents/configuration/test/rofibot.cpp
@@ -5,21 +5,6 @@
 
 namespace {
 
-void printMatrix( Matrix m ) {
-    std::cout << "-------------------\n";
-    std::cout << m << "\n";
-    std::cout << "-------------------\n";
-    /*
-    for ( int i = 0; i < m.n_rows; i++ ) {
-        for ( int j = 0; j < m.n_cols; j++ ) {
-            std::cout << round( m(i, j) ) << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-    */
-}
-
 TEST_CASE( "Base Module Test" ) {}
 
 TEST_CASE( "Universal Module Test" ) {
@@ -27,52 +12,195 @@ TEST_CASE( "Universal Module Test" ) {
         rofi::Module um = rofi::buildUniversalModule( 0, 0, 0 );
         CHECK( um.connectors().size() - 1 == 6 );
         um.setJointParams( 0, { 0, 0, 0 } );
+        CHECK( um.components().size() -1 == 9 );
     }
 
     SECTION( "Position - default" ) {
         rofi::Module um = rofi::buildUniversalModule( 0, 0, 0 );
+        // A part
+        CHECK( equals( um.getComponentPosition( 0 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 1 ), rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        CHECK( equals( um.getComponentPosition( 2 ), rotate( - M_PI_2, { 0, 1, 0 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
         CHECK( equals( um.getComponentPosition( 6 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 7 ), identity ) );
+        // B part
+        // -X
+        CHECK( equals( um.getComponentPosition( 3 ), translate( { 0, 0, 1 } ) * rotate( M_PI, { 0, 0, 1 } )
+                                                   * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // +X
+        CHECK( equals( um.getComponentPosition( 4 ), translate( { 0, 0, 1 } ) * rotate( M_PI, { 0, 1, 0 } )
+                                                   * rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // -Z
+        CHECK( equals( um.getComponentPosition( 5 ), translate( { 0, 0, 1 } ) * rotate( M_PI, { 0, 1, 0 } )
+                                                   * rotate( - M_PI_2, { 0, 1, 0 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // Body
+        CHECK( equals( um.getComponentPosition( 8 ), translate( { 0, 0, 1 } ) * rotate( M_PI, { 0, 1, 0 } ) ) );
+        // Shoe
         CHECK( equals( um.getComponentPosition( 9 ), { { -1, 0,  0, 0 }
                                                      , {  0, 1,  0, 0 }
                                                      , {  0, 0, -1, 1 }
                                                      , {  0, 0,  0, 1 } } ) );
-        printMatrix( um.getComponentPosition( 9 ) );
     }
 
-    SECTION( "Position - rotated" ) {
-        rofi::Module um = rofi::buildUniversalModule( 0, -M_PI_2, M_PI_2 );
-        CHECK( equals( um.getComponentPosition( 6 ), identity ) ); // shoeA
-        CHECK( equals( um.getComponentPosition( 9 ), { { 0 , 0 , -1, 0 } // shoeB
-                                                     , { -1, 0 , 0 , 0 }
-                                                     , { 0 , 1 , 0 , 1 }
-                                                     , { 0 , 0 , 0 , 1 } } ) );
-        printMatrix( um.getComponentPosition( 9 ) );
+    SECTION( "Position - rotated gamma" ) {
+        rofi::Module um = rofi::buildUniversalModule( 0, 0, 90 );
+        // A part
+        CHECK( equals( um.getComponentPosition( 0 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 1 ), rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        CHECK( equals( um.getComponentPosition( 2 ), rotate( - M_PI_2, { 0, 1, 0 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        CHECK( equals( um.getComponentPosition( 6 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 7 ), identity ) );
+        // B part
+        // -X
+        CHECK( equals( um.getComponentPosition( 3 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // +X
+        CHECK( equals( um.getComponentPosition( 4 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                   * rotate( M_PI, { 0, 1, 0 } ) * rotate( M_PI, { 0, 0, 1 } )
+                                                   * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // -Z
+        CHECK( equals( um.getComponentPosition( 5 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 1, 0 } ) * rotate( - M_PI_2, { 0, 1, 0 } )
+                                                    * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // Body
+        CHECK( equals( um.getComponentPosition( 8 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                   * rotate( M_PI, { 0, 1, 0 } ) ) );
+        // Shoe
+        CHECK( equals( um.getComponentPosition( 9 ), { {  0, -1,  0, 0 } // shoeB
+                                                     , { -1,  0,  0, 0 }
+                                                     , {  0,  0, -1, 1 }
+                                                     , {  0,  0,  0, 1 } } ) );
+    }
+
+    SECTION( "Position - rotated beta + gamma" ) {
+        rofi::Module um = rofi::buildUniversalModule( 0, -90, 90 );
+        // A part
+        CHECK( equals( um.getComponentPosition( 0 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 1 ), rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        CHECK( equals( um.getComponentPosition( 2 ), rotate( - M_PI_2, { 0, 1, 0 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        CHECK( equals( um.getComponentPosition( 6 ), identity ) );
+        CHECK( equals( um.getComponentPosition( 7 ), identity ) );
+        // B part
+        // -X
+        CHECK( equals( um.getComponentPosition( 3 ), rotate( M_PI_2, { 0, 0, 1 } )
+                                                    * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 1, 0 } )
+                                                    * rotate( - M_PI_2, { 1, 0, 0 } ) ) );
+        // +X
+        CHECK( equals( um.getComponentPosition( 4 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 1, 0 } )  * rotate( - M_PI_2, { 1, 0, 0 } )
+                                                    * rotate( M_PI, { 0, 0, 1 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // -Z
+        CHECK( equals( um.getComponentPosition( 5 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 1, 0 } )  * rotate( - M_PI_2, { 1, 0, 0 } )
+                                                    * rotate( - M_PI_2, { 0, 1, 0 } ) * rotate( M_PI, { 1, 0, 0 } ) ) );
+        // Body
+        CHECK( equals( um.getComponentPosition( 8 ), rotate( M_PI_2, { 0, 0, 1 } ) * translate( { 0, 0, 1 } )
+                                                    * rotate( M_PI, { 0, 1, 0 } ) ) );
+        // Shoe
+        CHECK( equals( um.getComponentPosition( 9 ), { {  0, 0, -1, 0 }
+                                                     , { -1, 0,  0, 0 }
+                                                     , {  0, 1,  0, 1 }
+                                                     , {  0, 0,  0, 1 } } ) );
     }
 }
 
-TEST_CASE( "Two modules" ) {
+TEST_CASE( "Two modules next to each other" ) {
     rofi::Rofibot bot;
     auto& m1 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
     auto& m2 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
-    connect( m1.connector( 4 ), m2.connector( 1 ), rofi::Orientation::North );
-    // ToDo: fix below
-    // rofi::connect< rofi::RigidJoint >( m1.connector( 1 ), { 0, 0, 0 }, identity );
-    CHECK_NOTHROW( bot.prepare() );
+    connect( m1.connector( 0 ), m2.connector( 2 ), rofi::Orientation::South );
+    rofi::connect< rofi::RigidJoint >( m1.body( 0 ), { 0, 0, 0 }, identity );
+    REQUIRE_NOTHROW( bot.prepare() );
 
-    SECTION( "ShoesA " ) {
-        CHECK( equals( bot.getModuleOrientation( m1.id ), identity ) );
-        CHECK( equals( bot.getModuleOrientation( m2.id ), identity * translate( { 0, 0, 1 } ) ) );
+    SECTION( "The second is just moved to left by one" ) {
+        Matrix new_origin = identity * translate( { -1, 0, 0 } );
+        REQUIRE( !equals( identity, new_origin ) );
+        Matrix mat = identity;
+        CHECK( equals( center( m2.getComponentPosition( 0, bot.getModulePosition( m2.id ) ) )
+                     , center( new_origin ) ) );
+
+        mat = new_origin * rotate( M_PI, { 0, 0, 1 } );
+        CHECK( equals( center( m2.getComponentPosition( 1, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = new_origin * rotate( - M_PI_2, { 0, 1, 0 } );
+        CHECK( equals( center( m2.getComponentPosition( 2, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+        CHECK( equals( center( m2.getComponentPosition( 6, bot.getModulePosition( m2.id ) ) ), center( new_origin ) ) );
+        CHECK( equals( center( m2.getComponentPosition( 7, bot.getModulePosition( m2.id ) ) ), center( new_origin ) ) );
+
+        mat =  new_origin * m1.getComponentPosition( 3 );
+        CHECK( equals( center( m2.getComponentPosition( 3, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = new_origin * m1.getComponentPosition( 4 );
+        CHECK( equals( center( m2.getComponentPosition( 4, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = new_origin * m1.getComponentPosition( 5 );
+        CHECK( equals( center( m2.getComponentPosition( 5, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = new_origin * m1.getComponentPosition( 8 );
+        CHECK( equals( center( m2.getComponentPosition( 8, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = { { -1, 0, 0, -1 }, { 0, 1, 0, 0 }, { 0, 0, -1, 1 }, { 0, 0, 0, 1 } };
+        CHECK( equals( center( m2.getComponentPosition( 9, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+
+        mat = new_origin * m1.getComponentPosition( 9 );
+        CHECK( equals( center( m2.getComponentPosition( 9, bot.getModulePosition( m2.id ) ) ), center( mat ) ) );
+    }
+}
+
+TEST_CASE( "Two modules - different angles" ) {
+    rofi::Rofibot bot;
+    auto& m1 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
+    auto& m2 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
+    connect( m1.connector( 3 ), m2.connector( 0 ), rofi::Orientation::South );
+    rofi::connect< rofi::RigidJoint >( m1.body( 0 ), { 0, 0, 0 }, identity );
+    REQUIRE_NOTHROW( bot.prepare() );
+
+    SECTION( "BodyA " ) {
+        CHECK( equals( bot.getModulePosition( m1.id ), identity ) );
+        CHECK( equals( center( bot.getModulePosition( m2.id ) ), center( identity * translate( { 1, 0, 1 } ) ) ) );
     }
 
-    SECTION( "ShoesB" ) {
-    Matrix m2shoeB = bot.getModuleOrientation( m2.id ) * m2.getComponentPosition( 9 );
-    Matrix m1shoeB = bot.getModuleOrientation( m1.id ) * m1.getComponentPosition( 9 );
-    printMatrix( bot.getModuleOrientation( m2.id ) );
-    CHECK( equals( m1shoeB, { { 0 , 0 , -1, 0 }
-                            , { -1, 0 , 0 , 0 }
-                            , { 0 , 1 , 0 , 1 }
-                            , { 0 , 0 , 0 , 1 } } ) );
-    CHECK( equals( m2shoeB, m1shoeB * translate( { 0, 0, 1 } ) ) );
+    SECTION( "BodyB" ) {
+    Matrix m1shoeB = m1.getComponentPosition( 9 );
+    Matrix m2shoeB = m2.getComponentPosition( 9, bot.getModulePosition( m2.id ) );
+    CHECK( equals( m1shoeB, { { -1, 0,  0, 0 }
+                            , {  0, 1,  0, 0 }
+                            , {  0, 0, -1, 1 }
+                            , {  0, 0,  0, 1 } } ) );
+    CHECK( equals( center( m2shoeB ), center( translate( { 1, 0, 2 } ) ) ) );
+    }
+}
+
+TEST_CASE( "Three modules -- connect docks 3 to 0s " ) {
+    rofi::Rofibot bot;
+    auto& m1 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
+    auto& m2 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
+    auto& m3 = bot.insert( rofi::buildUniversalModule( 0, 0, 0 ) );
+    connect( m1.connector( 3 ), m2.connector( 0 ), rofi::Orientation::South );
+    connect( m2.connector( 3 ), m3.connector( 0 ), rofi::Orientation::South );
+    rofi::connect< rofi::RigidJoint >( m1.body( 0 ), { 0, 0, 0 }, identity );
+    REQUIRE_NOTHROW( bot.prepare() );
+
+    SECTION( "Modules are well placed" ) {
+        CHECK( equals( center( bot.getModulePosition( m1.id ) ), center( identity ) ) );
+        CHECK( equals( center( bot.getModulePosition( m2.id ) ), center( translate( { 1, 0, 1 } ) ) ) );
+        CHECK( equals( center( bot.getModulePosition( m3.id ) ), center( translate( { 2, 0, 2 } ) ) ) );
+    }
+
+    SECTION( "Shoes A" ) {
+        CHECK( equals( m1.getComponentPosition( 6, bot.getModulePosition( m1.id ) ), identity ) );
+        CHECK( equals( m2.getComponentPosition( 6, bot.getModulePosition( m2.id ) ), translate( { 1, 0, 1 } ) ) );
+        CHECK( equals( m3.getComponentPosition( 6, bot.getModulePosition( m3.id ) ), translate( { 2, 0, 2 } ) ) );
+    }
+
+    SECTION( "Shoes B" ) {
+        CHECK( equals( m1.getComponentPosition( 9, bot.getModulePosition( m1.id ) )
+                     , translate( { 0, 0, 1 } ) * rotate( M_PI, { 0, 1, 0 } ) ) );
+        CHECK( equals( m2.getComponentPosition( 9, bot.getModulePosition( m2.id ) )
+                     , translate( { 1, 0, 2 } ) * rotate( M_PI, { 0, 1, 0 } ) ) );
+        CHECK( equals( m3.getComponentPosition( 9, bot.getModulePosition( m3.id ) )
+                     , translate( { 2, 0, 3 } ) * rotate( M_PI, { 0, 1, 0 } ) ) );
     }
 }
 
@@ -97,10 +225,18 @@ TEST_CASE( "Basic rofibot manipulation" ) {
     REQUIRE( m4.id == 3 );
     auto& m5 = bot.insert( buildUniversalModule( 0, 0, 0 ) );
     REQUIRE( m5.id == 4 );
+    CHECK( bot.modules().size() == 5 );
 
-    connect( m1.connector( 3 ), m3.connector( 2 ), rofi::Orientation::North );
-    m1.setJointParams( 0, { 0, 0, 0 } );
+    CHECK( bot.roficoms().size() == 0 );
+    connect( m1.connector( 3 ), m2.connector( 2 ), rofi::Orientation::North );
+    connect( m2.connector( 3 ), m3.connector( 2 ), rofi::Orientation::North );
+    connect( m3.connector( 3 ), m4.connector( 2 ), rofi::Orientation::North );
+    connect( m4.connector( 3 ), m5.connector( 2 ), rofi::Orientation::North );
+    CHECK( bot.roficoms().size() == 4 );
+    rofi::connect< rofi::RigidJoint >( m1.body( 0 ), { 0, 0, 0 }, identity );
+    m1.setJointParams( 2, { 90 } );
     auto [ b, str ] = bot.isValid( SimpleColision() );
+    CHECK( b );
     if ( !b )
         std::cout << "Error: " << str << "\n";
 }
