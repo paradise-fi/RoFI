739af4f808d84b7bc5cf862dbdfb4a2a78f17b79
Vladimír Chlup <xchlup2@fi.muni.cz>
2021-10-06 15:16:02 +0200

    tools: Change rofiTool to work with the rofi::configuration namespace

--- tools/rofiTool/check.hpp
+++ tools/rofiTool/check.hpp
@@ -1,5 +1,4 @@
 #include <dimcli/cli.h>
-#include <universalModule.hpp>
 
 static auto command = Dim::Cli().command( "check" )
     .desc( "Check a given configuration" );
@@ -7,19 +6,4 @@ static auto& inputFile = command.opt< std::string >( "<FILE>" )
     .desc("Specify configuration file");
 
 
-int check( Dim::Cli & cli ) {
-    auto cfgFile = std::ifstream( *inputFile );
-    if ( !cfgFile.is_open() )
-        throw std::runtime_error( "Cannot open file '" + *inputFile + "'" );
-
-    auto configuration = rofi::readOldConfigurationFormat( cfgFile );
-    rofi::connect< rofi::RigidJoint >(
-        configuration.getModule( 0 )->body( 0 ),
-        Vector( { 0, 0, 0 } ),
-        identity );
-    auto [ ok, str ] = configuration.isValid( rofi::SimpleColision() );
-    if ( !ok )
-        throw std::runtime_error( str );
-    return 0;
-}
-
+int check( Dim::Cli & cli );
