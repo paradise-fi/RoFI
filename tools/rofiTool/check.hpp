921a8c787e3f9aede67fe403a7f374a92bf75f78
Vladimír Chlup <xchlup2@fi.muni.cz>
2021-10-13 14:47:10 +0200

    tools: Move static things from a header file in rofiTool

--- tools/rofiTool/check.hpp
+++ tools/rofiTool/check.hpp
@@ -1,9 +1,3 @@
 #include <dimcli/cli.h>
 
-static auto command = Dim::Cli().command( "check" )
-    .desc( "Check a given configuration" );
-static auto& inputFile = command.opt< std::string >( "<FILE>" )
-    .desc("Specify configuration file");
-
-
 int check( Dim::Cli & cli );
