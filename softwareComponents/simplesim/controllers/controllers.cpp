e6ea42fe6dc8a3d77e7f0bbffad3a6edfad4071b
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2021-10-17 23:10:57 +0200

    simplesim: Rename rofi interface to communication

--- softwareComponents/simplesim/controllers/controllers.cpp
+++ softwareComponents/simplesim/controllers/controllers.cpp
@@ -4,22 +4,22 @@
 namespace rofi::simplesim
 {
 Controller Controller::runRofiController( std::shared_ptr< Simulation > simulation,
-                                          std::shared_ptr< RofiInterface > rofiInterface )
+                                          std::shared_ptr< Communication > communication )
 {
     assert( simulation );
-    assert( rofiInterface );
+    assert( communication );
 
     return Controller( std::jthread( &Controller::rofiControllerThread,
                                      std::move( simulation ),
-                                     std::move( rofiInterface ) ) );
+                                     std::move( communication ) ) );
 }
 
-void processRofiCommands( Simulation & simulation, RofiInterface & rofiInterface )
+void processRofiCommands( Simulation & simulation, Communication & communication )
 {
-    auto rofiCommands = rofiInterface.getRofiCommands();
+    auto rofiCommands = communication.getRofiCommands();
     for ( auto & rofiCommand : rofiCommands ) {
         if ( auto response = simulation.processRofiCommand( *rofiCommand ) ) {
-            rofiInterface.sendRofiResponses( std::array{ *response } );
+            communication.sendRofiResponses( std::array{ *response } );
         }
     }
 }
@@ -27,19 +27,19 @@ void processRofiCommands( Simulation & simulation, RofiInterface & rofiInterface
 
 void Controller::rofiControllerThread( std::stop_token stopToken,
                                        std::shared_ptr< Simulation > simulationPtr,
-                                       std::shared_ptr< RofiInterface > rofiInterfacePtr )
+                                       std::shared_ptr< Communication > communicationPtr )
 {
     assert( simulationPtr );
-    assert( rofiInterfacePtr );
+    assert( communicationPtr );
     auto & simulation = *simulationPtr;
-    auto & rofiInterface = *rofiInterfacePtr;
+    auto & communication = *communicationPtr;
 
     while ( !stopToken.stop_requested() ) {
         auto startTime = std::chrono::steady_clock::now();
 
-        processRofiCommands( simulation, rofiInterface );
+        processRofiCommands( simulation, communication );
         auto eventResponses = simulation.simulateOneIteration();
-        rofiInterface.sendRofiResponses( std::move( eventResponses ) );
+        communication.sendRofiResponses( std::move( eventResponses ) );
 
         std::this_thread::sleep_until( startTime + Controller::updateDuration );
     }
