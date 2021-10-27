1db8b355b65ef729ab1a2d180479c6a187715903
Vladimír Chlup <xchlup2@fi.muni.cz>
2021-10-27 12:54:02 +0200

    configuration: Fix constructors and tests for derived modules

--- softwareComponents/configuration/pad.cpp
+++ softwareComponents/configuration/pad.cpp
@@ -1,35 +1,5 @@
 #include "pad.hpp"
 
 namespace rofi::configuration {
-
-Module Pad::buildPad( int size ) {
-    return buildPad( size, size );
-}
-
-Module Pad::buildPad( int sizeN, int sizeM ) {
-    assert( sizeN > 0 && sizeM > 0 && "buildPad has to get positive dimensions" );
-    std::vector< Component > components( sizeN * sizeM, Component { ComponentType::Roficom } );
-
-    std::vector< ComponentJoint > joints;
-    
-    for ( int i = 0; i < sizeN; i++ ) {
-        for ( int j = 0; j < sizeM; j++ ) {
-            if ( i > 0 ) {
-                joints.push_back(
-                    makeComponentJoint< RigidJoint >( (i - 1) * sizeM + j, i * sizeM + j, translate( { 0, 1, 0 } ) )
-                );
-            }
-
-            if ( j > 0 ) {
-                joints.push_back(
-                    makeComponentJoint< RigidJoint >( i * sizeM + j - 1, i * sizeM + j, translate( { 0, 0, 1 } ) )
-                );
-            }
-        }
-    }
-
-    return Module( ModuleType::Pad, std::move( components ), sizeN * sizeM,
-        std::move( joints ), 0 );
-}
-
+    // TODO: Probably delete this
 } // namespace rofi::configuration
--- softwareComponents/configuration/pad.hpp
+++ softwareComponents/configuration/pad.hpp
@@ -7,15 +7,43 @@
 namespace rofi::configuration {
 
 class Pad : public Module {
-    Module buildPad( int sizeN, int sizeM );
-    Module buildPad( int size );
+
+    static std::vector< Component > _initializeComponents( int size ) {
+        return std::vector< Component >( size, Component { ComponentType::Roficom } );
+    }
+
+    static std::vector< ComponentJoint > _initializeJoints( int n, int m ) {
+        std::vector< ComponentJoint > joints;
+        for ( int i = 0; i < n; i++ ) {
+            for ( int j = 0; j < m; j++ ) {
+                if ( i > 0 ) {
+                    joints.push_back(
+                        makeComponentJoint< RigidJoint >( (i - 1) * m + j, i * m + j, translate( { 0, 1, 0 } ) )
+                    );
+                }
+
+                if ( j > 0 ) {
+                    joints.push_back(
+                        makeComponentJoint< RigidJoint >( i * m + j - 1, i * m + j, translate( { 0, 0, 1 } ) )
+                    );
+                }
+            }
+        }
+        return joints;
+    }
 
 public:
-    Pad( int sizeN, int sizeM ) : Module( buildPad( sizeN, sizeM ) ) {};
+    Pad( int sizeN, int sizeM )
+    : Module( ModuleType::Pad, _initializeComponents( sizeN * sizeM )
+    , sizeN * sizeM, _initializeJoints( sizeN, sizeM ), 0 ) {
+        assert( sizeN > 0 && sizeM > 0 && "buildPad has to get positive dimensions" );
+    };
 
-    explicit Pad( int size ) : Module( buildPad( size ) ) {};
+    explicit Pad( int size ) : Pad( size, size ) {};
 
-    virtual ~Pad() = default;
+    ~Pad() override = default;
+
+    ATOMS_CLONEABLE( Pad );
 };
 
 
