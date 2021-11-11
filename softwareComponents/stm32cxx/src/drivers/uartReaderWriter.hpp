45f7be45181e2ec5955391c6717885707e48eeb0
Jan Mrázek <email@honzamrazek.cz>
2021-11-11 17:09:04 +0100

    stm32cxx: Implement circular mode for uartReader

--- softwareComponents/stm32cxx/src/drivers/uartReaderWriter.hpp
+++ softwareComponents/stm32cxx/src/drivers/uartReaderWriter.hpp
@@ -14,6 +14,7 @@ public:
     UartReader( UartD& uart, Dma::Channel channel ) : _uart( uart ) {
         _channel = std::move( channel );
         assert( _channel );
+        _channel.disable();
 
         if constexpr ( Dma::supportsMuxing ) {
             LL_DMA_SetPeriphRequest( _channel, _channel, LL_DMAMUX_REQ_RX( _uart.periph() ) );
@@ -24,13 +25,12 @@ public:
 
         LL_DMA_SetDataTransferDirection( _channel, _channel, LL_DMA_DIRECTION_PERIPH_TO_MEMORY );
         _channel.setPriority( LL_DMA_PRIORITY_LOW );
-        LL_DMA_SetMode( _channel, _channel, LL_DMA_MODE_NORMAL );
         LL_DMA_SetPeriphIncMode( _channel, _channel, LL_DMA_PERIPH_NOINCREMENT );
         LL_DMA_SetMemoryIncMode( _channel, _channel, LL_DMA_MEMORY_INCREMENT );
-        LL_DMA_SetPeriphSize( _channel, _channel, LL_DMA_PDATAALIGN_BYTE);
-        LL_DMA_SetMemorySize( _channel, _channel, LL_DMA_MDATAALIGN_BYTE);
+        LL_DMA_SetPeriphSize( _channel, _channel, LL_DMA_PDATAALIGN_BYTE );
+        LL_DMA_SetMemorySize( _channel, _channel, LL_DMA_MDATAALIGN_BYTE );
 
-        LL_DMA_SetPeriphAddress( _channel, _channel, _uart.getDmaTxAddr() );
+        LL_DMA_SetPeriphAddress( _channel, _channel, _uart.getDmaRxAddr() );
         LL_USART_EnableDMAReq_RX( _uart.periph() );
 
         _channel.enableInterrupt();
@@ -38,7 +38,9 @@ public:
 
     template < typename Callback >
     void readBlock( Mem block, int offset, int size, int bitTimeout, Callback callback ) {
+        assert( !_channel.isEnabled() );
         _block = std::move( block );
+        LL_DMA_SetMode( _channel, _channel, LL_DMA_MODE_NORMAL );
         LL_DMA_SetMemoryAddress( _channel, _channel, uint32_t( _block.get() + offset ) );
         LL_DMA_SetDataLength( _channel, _channel, size );
 
@@ -76,21 +78,61 @@ public:
     }
 
     void startBufferedReading( int bufferSize = 256 ) {
+        assert( !_channel.isEnabled() );
         _buffer = RingBuffer< char, Allocator >(
             Allocator::allocate( bufferSize ), bufferSize );
         _updateBuffer( 0 );
     }
 
+    void stopBufferedReading() {
+        _channel.disable();
+        _buffer = {};
+    }
+
     bool available() const {
+        assert( _buffer.capacity() != 0 );
         return !_buffer.empty();
     }
 
     int size() const {
+        assert( _buffer.capacity() != 0 );
         return _buffer.size();
     }
 
     char get() {
+        assert( _buffer.capacity() != 0 );
         return _buffer.pop_front();
+    }
+
+    template < typename F >
+    void startCircularReading( int bufferSize, int bitTimeout, F handleNewData ) {
+        assert( !_channel.isEnabled() );
+
+        _block = Allocator::allocate( bufferSize );
+        assert( _block );
+
+        LL_DMA_SetMode( _channel, _channel, LL_DMA_MODE_CIRCULAR );
+        LL_DMA_SetMemoryAddress( _channel, _channel, uint32_t( _block.get() ) );
+        LL_DMA_SetDataLength( _channel, _channel, bufferSize );
+
+        _lastPos = 0;
+        _newDataHandler = handleNewData;
+        _bufferSize = bufferSize;
+        _channel.onHalf( [this]() {
+            _notifyCircular();
+        } );
+        _channel.onComplete( [this]() {
+            _notifyCircular();
+        } );
+        _onTimeout( bitTimeout, [this]() {
+            _notifyCircular();
+        } );
+
+        _channel.enable();
+    }
+
+    void stopCircularReading() {
+        _channel.disable();
     }
 
 private:
@@ -98,6 +140,7 @@ private:
         _buffer.advance( read );
         auto [ location, size ] = _buffer.insertPosition();
         int requested = std::min( size, 32 );
+        LL_DMA_SetMode( _channel, _channel, LL_DMA_MODE_NORMAL );
         LL_DMA_SetMemoryAddress( _channel, _channel, uint32_t( location ) );
         LL_DMA_SetDataLength( _channel, _channel, requested );
         if constexpr ( UartD::supportsTimeout ) {
@@ -117,10 +160,42 @@ private:
         _channel.enable();
     }
 
+    template < typename F >
+    void _onTimeout( int bitTimeout, F f ) {
+        if ( bitTimeout == 0 ) {
+            _uart.disableTimeout();
+            return;
+        }
+        if constexpr ( UartD::supportsTimeout ) {
+            _uart.enableTimeout( bitTimeout, f );
+        }
+        else if constexpr ( UartD::supportsIdle ) {
+            assert( bitTimeout == 8 && "UART only supports idle detection" );
+            _uart.enableTimeout( f );
+        }
+        else {
+            assert( bitTimeout == 0 && "UART does not support bit timeout" );
+        }
+    }
+
+    void _notifyCircular() {
+        int end = _bufferSize - LL_DMA_GetDataLength( _channel, _channel );
+        if ( _lastPos > end ) { // We moved pass the buffer boundary
+            int size = _bufferSize - _lastPos;
+            _newDataHandler( &_block[ _lastPos ], size );
+            _lastPos = 0;
+        }
+        int size = end - _lastPos;
+        _newDataHandler( &_block[ _lastPos ], size );
+        _lastPos = end;
+    }
+
     UartD& _uart;
     Dma::Channel _channel;
     RingBuffer< char, Allocator > _buffer;
     Mem _block;
+    int _lastPos, _bufferSize;
+    std::function< void( const uint8_t*, int size ) > _newDataHandler;
 };
 
 template < typename Reader >
