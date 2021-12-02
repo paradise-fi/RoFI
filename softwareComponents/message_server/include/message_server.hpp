d7dadb11d6a3cef0678fce5d523d23a435a7994e
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2021-12-02 11:36:10 +0100

    simplesim: Revert changes on message-server

--- softwareComponents/message_server/include/message_server.hpp
+++ softwareComponents/message_server/include/message_server.hpp
@@ -1,37 +1,26 @@
 #pragma once
 
 #include <memory>
-#include <string_view>
+#include <string>
+
+#include <gazebo/Master.hh>
 
 
 namespace rofi::msgs
 {
-namespace details
-{
-    class MessageServerImpl;
-}
-
-
 class [[nodiscard]] MessageServer
 {
 public:
-    MessageServer( std::string_view logName = "default" );
-    MessageServer( MessageServer && ) noexcept; // = default;
-    ~MessageServer(); // = default;
+    // MessageServer( std::string_view logName = "default" );
 
-    void poll();
-    void loop();
-    void loopInThread();
+    // void poll();
+    // void loop();
+    // void loopInThread();
 
-    static MessageServer createAndLoopInThread( std::string_view logName = "default" )
-    {
-        auto result = MessageServer( logName );
-        result.loopInThread();
-        return std::move( result );
-    }
+    static MessageServer createAndLoopInThread( std::string_view logName = "default" );
 
 private:
-    std::unique_ptr< details::MessageServerImpl > _impl;
+    std::unique_ptr< gazebo::Master > _impl;
 };
 
 } // namespace rofi::msgs
