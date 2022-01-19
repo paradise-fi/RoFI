37fa635b8717d573b499bcda5edcf5644000a496
Jan Mrázek <email@honzamrazek.cz>
2022-01-19 19:43:19 +0100

    releng: Add support for clang-tidy to rmake

--- releng/tools/common.py
+++ releng/tools/common.py
@@ -58,3 +58,29 @@ def availableTargets():
         for path, target in [x.strip().split(":") for x in suiteTargets]:
             targets.append(TargetRecord(target, path, suite))
     return targets
+
+def dependencyList():
+    deps = {}
+    for suite in configuredSuites():
+        try:
+            with open(os.path.join(buildDir, suite, "dependencies.txt")) as f:
+                for l in f.readlines():
+                    t, d = tuple(l.split(": "))
+                    d = [x.strip() for x in d.strip().split(";") if len(x.strip()) > 0]
+                    deps[t] = d
+        except Exception as e:
+            pass # Ingore uncofigured targets
+    return deps
+
+def sourceList():
+    sources = {}
+    for suite in configuredSuites():
+        try:
+            with open(os.path.join(buildDir, suite, "sources.txt")) as f:
+                for l in f.readlines():
+                    t, s = tuple(l.split(": "))
+                    s = [x.strip() for x in s.strip().split(";") if len(x.strip()) > 0]
+                    sources[t] = s
+        except Exception as e:
+            pass # Ingore uncofigured targets
+    return sources
