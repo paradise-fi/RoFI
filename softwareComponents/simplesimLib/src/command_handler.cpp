9a641b678684663c41f5100fd0c6a8669ea6f178
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-03-23 02:24:13 +0100

    simplesim: Add sender to SendPacketEvent

--- softwareComponents/simplesimLib/src/command_handler.cpp
+++ softwareComponents/simplesimLib/src/command_handler.cpp
@@ -222,6 +222,8 @@ CommandHandler::DelayedEvent sendConnectorPacket( ModuleStates & moduleStates,
     if ( auto connectedToOpt = moduleStates.getConnectedTo( moduleId, connector ) ) {
         if ( auto connectedTo = *connectedToOpt ) {
             return CommandHandler::SendPacketEvent{
+                    .sender = CommandHandler::Connector{ .moduleId = moduleId,
+                                                           .connector = connector },
                     .receiver = CommandHandler::Connector{ .moduleId = connectedTo->moduleId,
                                                            .connector = connectedTo->connector },
                     .packet = rofiCmd.connectorcmd().packet() };
