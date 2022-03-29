70c1a8d669a642a57b541f5f85fbe15028fd669f
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-03-29 16:40:01 +0200

    simplesim-client: Remake parsing input

--- softwareComponents/simplesimClientLib/src/changecolor.cpp
+++ softwareComponents/simplesimClientLib/src/changecolor.cpp
@@ -20,19 +20,16 @@ ChangeColor::ChangeColor( QWidget * parent, size_t size )
 
 ChangeColor::~ChangeColor() {}
 
-struct syntaxError : std::exception {};
-struct rangeError : std::exception {};
-
 void ChangeColor::accept()
 {
     hide();
     std::fill( _toColor.begin(), _toColor.end(), false );
     try {
         parseInput();
-    } catch ( syntaxError & e ) {
-        std::cerr << "Couldn't parse input\n";
-    } catch ( rangeError & e ) {
-        std::cerr << "Given module out of range\n";
+    } catch ( const std::invalid_argument & e ) {
+        std::cerr << "Couldn't parse input (" << e.what() << ")\n";
+    } catch ( const std::out_of_range & e ) {
+        std::cerr << "Input out of range (" << e.what() << ")\n";
     }
 
     _ui->textEdit->setPlainText( "" );
@@ -57,49 +54,41 @@ void ChangeColor::toggleHelp()
 
 void ChangeColor::parseInput()
 {
-    QString source = _ui->textEdit->toPlainText();
-    int i = 0;
-    int number = -1;
+    auto parseUInt = []( QStringRef numStr ) {
+        bool ok = true;
+        auto result = numStr.trimmed().toUInt( &ok );
+        if ( !ok ) {
+            throw std::invalid_argument( "expected number (uint)" );
+        }
+        return result;
+    };
 
-    auto parseNum = [ & ]() {
-        std::string buffer;
-        if ( !source[ i ].isDigit() ) {
-            throw syntaxError();
-        }
-        while ( i < source.size() && source[ i ].isDigit() ) {
-            buffer += source[ i++ ].toLatin1();
-        }
-        return std::stoi( buffer );
-    };
-    auto parseNext = [ & ]() {
-        _toColor[ number ] = true;
-        if ( source[ i++ ] == ',' ) {
-            number = parseNum();
-        }
-    };
-    auto parseRange = [ & ]() {
-        if ( source[ i++ ] != '.' || source[ i++ ] != '.' ) {
-            throw syntaxError();
-        }
-        int upper = parseNum();
-        if ( upper >= static_cast< int >( _toColor.size() ) ) {
-            throw syntaxError();
-        }
-        for ( int j = number; j <= upper; j++ ) {
-            _toColor[ j ] = true;
-        }
-    };
-    number = parseNum();
-    while ( i < source.size() ) {
-        if ( source[ i ] == ',' ) {
-            parseNext();
-        }
-        if ( source[ i ] == '.' ) {
-            parseRange();
+    QString source = _ui->textEdit->toPlainText();
+    for ( QStringRef part : source.splitRef( ',', QString::SkipEmptyParts ) ) {
+        if ( part.contains( ".." ) ) {
+            auto numberStrings = part.split( ".." );
+            if ( numberStrings.size() != 2 ) {
+                throw std::invalid_argument( "expected at most one '..' in range" );
+            }
+            auto start = parseUInt( numberStrings[ 0 ] );
+            auto end = parseUInt( numberStrings[ 1 ] );
+
+            if ( start > end ) {
+                throw std::invalid_argument( "invalid range" );
+            }
+            if ( end >= _toColor.size() ) {
+                throw std::out_of_range( "module range out of range" );
+            }
+
+            for ( auto i = start; i <= end; i++ ) {
+                _toColor[ i ] = true;
+            }
+        } else {
+            auto idx = parseUInt( part );
+            if ( idx >= _toColor.size() ) {
+                throw std::out_of_range( "module out of range" );
+            }
+            _toColor[ idx ] = true;
         }
     }
-    if ( number < 0 || to_unsigned( number ) >= _toColor.size() ) {
-        throw rangeError();
-    }
-    _toColor[ number ] = true;
 }
