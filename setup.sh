cf272b6a35eabb0bc20938718cc4c106de38a1eb
Jan Mrázek <email@honzamrazek.cz>
2022-04-12 16:12:04 +0200

    releng: Allow for prefix in a build directory

--- setup.sh
+++ setup.sh
@@ -211,7 +211,7 @@ run() {
 
     export ROFI_BUILD_CONFIGURATION
     export ROFI_ROOT=$(pwd)
-    export ROFI_BUILD_DIR="$(pwd)/build.${ROFI_BUILD_CONFIGURATION}"
+    export ROFI_BUILD_DIR="$(pwd)/build.${ROFI_BUILD_PREFIX}${ROFI_BUILD_CONFIGURATION}"
 
     export GAZEBO_MODEL_PATH="$ROFI_ROOT/data/gazebo/models:$GAZEBO_MODEL_PATH"
     export GAZEBO_PLUGIN_PATH="$ROFI_BUILD_DIR/desktop/lib:$GAZEBO_PLUGIN_PATH"
