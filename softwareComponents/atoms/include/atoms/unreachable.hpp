9196560c7e1df04fe1b97e8ee0fd1afe331d0220
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-05-05 16:43:26 +0200

    atoms: Add unreachable macro

---
+++ softwareComponents/atoms/include/atoms/unreachable.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cassert>
+
+
+#ifdef NDEBUG
+
+// `std::unreachable` from C++23
+#if defined( __GNUC__ ) // GCC, Clang, ICC
+#define ROFI_UNREACHABLE( description ) __builtin_unreachable()
+#elif defined( _MSC_VER ) // MSVC
+#define ROFI_UNREACHABLE( description ) __assume( false )
+#endif
+
+#else
+
+#define ROFI_UNREACHABLE( description ) assert( false && description )
+
+#endif
