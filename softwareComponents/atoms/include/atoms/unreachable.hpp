46646159646badfdfc5a4214fc469e9a14b150bd
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-05-11 21:44:37 +0200

    atoms: Format unreachable

--- softwareComponents/atoms/include/atoms/unreachable.hpp
+++ softwareComponents/atoms/include/atoms/unreachable.hpp
@@ -4,16 +4,12 @@
 
 
 #ifdef NDEBUG
-
-// `std::unreachable` from C++23
-#if defined( __GNUC__ ) // GCC, Clang, ICC
-#define ROFI_UNREACHABLE( description ) __builtin_unreachable()
-#elif defined( _MSC_VER ) // MSVC
-#define ROFI_UNREACHABLE( description ) __assume( false )
-#endif
-
+    // `std::unreachable` from C++23
+    #if defined( __GNUC__ ) // GCC, Clang, ICC
+        #define ROFI_UNREACHABLE( description ) __builtin_unreachable()
+    #elif defined( _MSC_VER ) // MSVC
+        #define ROFI_UNREACHABLE( description ) __assume( false )
+    #endif
 #else
-
-#define ROFI_UNREACHABLE( description ) assert( false && description )
-
+    #define ROFI_UNREACHABLE( description ) assert( false && description )
 #endif
