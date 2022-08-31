501d885a231c74f27960a03185a5057876d71715
Jan Mrázek <email@honzamrazek.cz>
2022-08-31 15:18:53 +0200

    util: Try to be a little verbose in measurement

---
+++ util/monitorMemUsage.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Monitors peak memory usage. Measures only memory resident in the physical
+# memory, ignores swapped pages
+
+set -e
+
+LOGFILE=$1
+shift
+
+echo Running command $@
+
+echo '{}' > $LOGFILE
+
+TFILE="$(mktemp /tmp/memUsage.XXXXXXXXX)" || exit 1
+
+/usr/bin/time -f '%M\n%e\n%S\n%U' -o ${TFILE} $@
+
+cat $LOGFILE | jq ". |= . + {
+    \"peakMemoryUsage\": $(sed '1q;d' ${TFILE}),
+    \"wallClockTime\": $(sed '2q;d' ${TFILE}),
+    \"kernelTime\": $(sed '3q;d' ${TFILE}),
+    \"userTime\": $(sed '4q;d' ${TFILE}),
+}" | sponge $LOGFILE
+
+rm ${TFILE}
+
+echo Finished
