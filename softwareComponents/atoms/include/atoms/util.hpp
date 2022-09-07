3b572bb9b16a07a074862574bcc9f8a3d7c75310
Jan Mrázek <email@honzamrazek.cz>
2022-09-07 12:32:39 +0200

    atoms: Implement defer

--- softwareComponents/atoms/include/atoms/util.hpp
+++ softwareComponents/atoms/include/atoms/util.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <type_traits>
+#include <functional>
 
 template < std::size_t... >
 struct sum: std::integral_constant< std::size_t, 0 > {};
 
 template <std::size_t X, std::size_t... Xs >
 struct sum< X, Xs... > :
-  std::integral_constant< std::size_t, X + sum< Xs... >::value > {};
+    std::integral_constant< std::size_t, X + sum< Xs... >::value > {};
 
 template< class... Ts >
 struct overload : Ts... { using Ts::operator()...; };
@@ -23,3 +24,21 @@ template < typename T >
 const T& as( const void* p ) {
     return *reinterpret_cast< const T * >( p );
 }
+
+
+/***
+ * Defer execution of a job until end of scope;
+ */
+class Defer {
+private:
+    std::function< void() > _job;
+public:
+    template < typename F >
+    Defer( F&& f ): _job( f ) {}
+    ~Defer() { _job(); }
+};
+
+
+#define CONCAT_( a, b ) a ## b
+#define CONCAT( a, b ) CONCAT_( a, b )
+#define ATOMS_DEFER( fn ) Defer CONCAT( __defer__, __LINE__ )( fn )
