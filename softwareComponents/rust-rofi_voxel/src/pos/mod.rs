a019d40d0fd1b43ae8a3ab3ee6e669f036bb7674
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-10-28 17:32:25 +0200

    voxel: Update VoxelWorld to allways be minimal size

--- softwareComponents/rust-rofi_voxel/src/pos/mod.rs
+++ softwareComponents/rust-rofi_voxel/src/pos/mod.rs
@@ -2,3 +2,20 @@ pub type IndexType = u8;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct VoxelPos(pub [IndexType; 3]);
+
+pub fn compute_minimal_pos_hull<TIndex, IPos>(
+    positions: IPos,
+) -> Option<[std::ops::Range<TIndex>; 3]>
+where
+    TIndex: Ord + std::ops::Add<TIndex, Output = TIndex> + num::One + Copy,
+    IPos: IntoIterator<Item = [TIndex; 3]>,
+{
+    positions
+        .into_iter()
+        .map(|pos| pos.map(|pos| pos..pos + TIndex::one()))
+        .reduce(|acc, pos| {
+            acc.zip(pos).map(|(acc, pos)| {
+                std::cmp::min(acc.start, pos.start)..std::cmp::max(acc.end, pos.end)
+            })
+        })
+}
--- softwareComponents/rust-rofi_voxel/src/serde/voxel_world.rs
+++ softwareComponents/rust-rofi_voxel/src/serde/voxel_world.rs
@@ -1,4 +1,3 @@
-use crate::pos::VoxelPos;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -7,34 +6,17 @@ pub struct VoxelWorld {
     bodies: Vec<super::Voxel>,
 }
 
-impl VoxelWorld {
-    fn get_sizes(&self) -> VoxelPos {
-        let mut sizes = [0; 3];
-        for voxel in &self.bodies {
-            let VoxelPos(voxel_pos) = voxel.pos;
-            for (size, &voxel_i) in sizes.iter_mut().zip(voxel_pos.iter()) {
-                *size = std::cmp::max(*size, voxel_i + 1);
-            }
-        }
-        VoxelPos(sizes)
-    }
-}
-
 impl From<&crate::voxel_world::VoxelWorld> for VoxelWorld {
     fn from(world: &crate::voxel_world::VoxelWorld) -> Self {
         Self {
-            bodies: world.all_bodies().map(super::Voxel::from).collect(),
+            bodies: world.all_bodies().map(Into::into).collect(),
         }
     }
 }
-impl From<&VoxelWorld> for crate::voxel_world::VoxelWorld {
-    fn from(world: &VoxelWorld) -> Self {
-        let mut result = Self::new(world.get_sizes());
-        for &voxel in &world.bodies {
-            let (body, pos) = crate::voxel::VoxelBodyWithPos::from(voxel);
-            *result.get_voxel_mut(pos).expect("Wrong size from world") =
-                crate::voxel::Voxel::new_with_body(body);
-        }
-        result
+impl TryFrom<&VoxelWorld> for crate::voxel_world::VoxelWorld {
+    type Error = crate::voxel_world::InvalidVoxelWorldError;
+
+    fn try_from(world: &VoxelWorld) -> Result<Self, Self::Error> {
+        Self::from_bodies(world.bodies.iter().copied().map(Into::into))
     }
 }
