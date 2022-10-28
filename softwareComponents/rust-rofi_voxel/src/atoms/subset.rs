19fcc389c53ff28d158d6ed60091d4c88d9f7536
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-10-28 19:37:57 +0200

    voxel: Add connectivity graph

---
+++ softwareComponents/rust-rofi_voxel/src/atoms/subset.rs
@@ -0,0 +1,45 @@
+#[derive(Debug, Clone)]
+pub struct Subset(u64);
+
+impl Subset {
+    pub const MAX_SIZE: usize = 64;
+
+    pub fn all(size: usize) -> Subset {
+        assert!(size <= Self::MAX_SIZE);
+        if size == Self::MAX_SIZE {
+            Self(u64::MAX)
+        } else {
+            Self((1u64 << size) - 1)
+        }
+    }
+
+    pub fn iter_all(size: usize) -> SubsetIter {
+        assert!(size <= Self::MAX_SIZE);
+        SubsetIter::new(Self::all(size))
+    }
+
+    pub fn has_elem(&self, elem: usize) -> bool {
+        assert!(elem < Self::MAX_SIZE);
+        self.0 & (1u64 << elem) != 0
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SubsetIter {
+    next: Option<Subset>,
+}
+
+impl SubsetIter {
+    fn new(subset: Subset) -> Self {
+        Self { next: Some(subset) }
+    }
+}
+
+impl Iterator for SubsetIter {
+    type Item = Subset;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.next.as_ref()?.0.checked_sub(1).map(Subset);
+        std::mem::replace(&mut self.next, new_next)
+    }
+}
---
+++ softwareComponents/rust-rofi_voxel/src/connectivity/graph.rs
@@ -0,0 +1,197 @@
+use super::get_bodies_connected_to_body;
+use crate::atoms::Subset;
+use crate::module_repr::{get_module_repr_pos, is_module_repr};
+use crate::pos::VoxelPos;
+use crate::voxel::body::get_neighbour_pos;
+use crate::voxel::VoxelBodyWithPos;
+use crate::voxel_world::{VoxelSubworld, VoxelWorld};
+use bimap::BiHashMap;
+use rs_graph::algorithms::{subgraph, Item};
+use rs_graph::linkedlistgraph::Node;
+use rs_graph::traits::{GraphSize, Undirected};
+use rs_graph::{Buildable, Builder, LinkedListGraph};
+use std::collections::HashMap;
+
+type GraphType = LinkedListGraph;
+
+pub struct ConnectivityGraph<'a> {
+    world: &'a VoxelWorld,
+    graph: GraphType,
+    mapping: BiHashMap<VoxelPos, Node>,
+}
+
+impl<'a> ConnectivityGraph<'a> {
+    fn add_connectivity_nodes(
+        world: &'a VoxelWorld,
+        mapping: &mut BiHashMap<VoxelPos, Node>,
+        graph_builder: &mut <GraphType as rs_graph::Buildable>::Builder,
+    ) {
+        for (_, module_pos) in world.all_bodies() {
+            let node_ids = graph_builder.add_node();
+            mapping.insert_no_overwrite(module_pos, node_ids).unwrap();
+        }
+    }
+
+    fn add_connectivity_edges(
+        world: &'a VoxelWorld,
+        mapping: &BiHashMap<VoxelPos, Node>,
+        graph_builder: &mut <GraphType as rs_graph::Buildable>::Builder,
+    ) {
+        for (&pos, &i) in mapping.iter() {
+            let body = world.get_body(pos).expect("Invalid world");
+
+            let other_body_pos = get_neighbour_pos((body, pos)).unwrap();
+            let &other_body_i = mapping.get_by_left(&other_body_pos).expect("Invalid world");
+            graph_builder.add_edge(i, other_body_i);
+
+            for (_, other_pos) in get_bodies_connected_to_body((body, pos), world) {
+                let &other_i = mapping.get_by_left(&other_pos).expect("Invalid mapping");
+                graph_builder.add_edge(i, other_i);
+            }
+        }
+    }
+
+    pub fn compute_from(world: &'a VoxelWorld) -> Self {
+        let mut graph_builder = GraphType::new_builder();
+        let mut mapping = BiHashMap::new();
+
+        Self::add_connectivity_nodes(world, &mut mapping, &mut graph_builder);
+        Self::add_connectivity_edges(world, &mapping, &mut graph_builder);
+
+        Self {
+            world,
+            graph: graph_builder.into_graph(),
+            mapping,
+        }
+    }
+
+    fn graph_with_separated_bodies_of(&self, module: VoxelBodyWithPos) -> GraphType {
+        assert!(is_module_repr(module.0));
+
+        let pos_a = module.1;
+        let pos_b = get_neighbour_pos(module).expect("Invalid world");
+        subgraph(&self.graph, |i| match i {
+            Item::Node(_) => true,
+            Item::Edge(e) => {
+                let enodes = self.graph.enodes(e);
+                let enodes = [enodes.0, enodes.1]
+                    .map(|e| *self.mapping.get_by_right(&e).expect("Node not in mapping"));
+                !enodes.contains(&pos_a) || !enodes.contains(&pos_b)
+            }
+        })
+    }
+
+    fn get_module_repr_node_to_id_mapping(
+        &self,
+        split_module_pos: VoxelPos,
+    ) -> (HashMap<Node, usize>, usize) {
+        let module_repr_pos_to_id = self
+            .mapping
+            .iter()
+            .map(|(&pos, _)| pos)
+            .filter(|&pos| {
+                let body = self.world.get_body(pos).expect("Invalid mapping");
+                is_module_repr(body) && pos != split_module_pos
+            })
+            .zip(0..)
+            .collect::<HashMap<_, _>>();
+
+        let id_count = module_repr_pos_to_id.len();
+
+        let module_repr_node_to_id_mapping = self
+            .mapping
+            .iter()
+            .filter_map(|(&pos, &node)| {
+                let body = self.world.get_body(pos).expect("Invalid mapping");
+                let module_repr_pos = get_module_repr_pos((body, pos));
+                module_repr_pos_to_id
+                    .get(&module_repr_pos)
+                    .map(|&id| (node, id))
+            })
+            .collect::<HashMap<_, _>>();
+
+        (module_repr_node_to_id_mapping, id_count)
+    }
+
+    fn get_is_selected_predicate(
+        &self,
+        split_module: VoxelBodyWithPos,
+    ) -> (impl Fn(Node, &Subset) -> bool + Clone + '_, usize) {
+        let (module_repr_node_to_id, id_count) =
+            self.get_module_repr_node_to_id_mapping(split_module.1);
+
+        let module_node = *self
+            .mapping
+            .get_by_left(&split_module.1)
+            .expect("Invalid mapping");
+        let other_module_body_pos = *self
+            .mapping
+            .get_by_left(&get_neighbour_pos(split_module).expect("Invalid world"))
+            .expect("Invalid mapping");
+
+        let is_selected = move |node: Node, selection: &Subset| -> bool {
+            if let Some(&node_index) = module_repr_node_to_id.get(&node) {
+                selection.has_elem(node_index)
+            } else if node == module_node {
+                true
+            } else {
+                assert_eq!(node, other_module_body_pos);
+                false
+            }
+        };
+        (is_selected, id_count)
+    }
+
+    pub fn all_cuts_by_module(
+        &self,
+        module: VoxelBodyWithPos,
+    ) -> impl Iterator<Item = VoxelSubworld> + '_ {
+        assert!(is_module_repr(module.0));
+
+        let (is_selected, id_count) = self.get_is_selected_predicate(module);
+
+        let graph_base = self.graph_with_separated_bodies_of(module);
+
+        debug_assert_eq!(self.graph.num_nodes(), 2 + id_count * 2);
+        Subset::iter_all(id_count).filter_map(move |selection| {
+            if Self::is_valid_selection(&graph_base, |node| is_selected(node, &selection)) {
+                let is_selected = is_selected.clone();
+                Some(VoxelSubworld::new(self.world, move |pos| {
+                    is_selected(
+                        *self.mapping.get_by_left(&pos).expect("Invalid mapping"),
+                        &selection,
+                    )
+                }))
+            } else {
+                None
+            }
+        })
+    }
+
+    fn get_subgraph<TSelection>(graph_base: &GraphType, selection: &TSelection) -> GraphType
+    where
+        TSelection: Fn(<GraphType as rs_graph::traits::GraphType>::Node) -> bool,
+    {
+        subgraph(graph_base, |i| match i {
+            Item::Node(node) => selection(node),
+            Item::Edge(_) => true,
+        })
+    }
+
+    fn is_valid_selection<TSelection>(graph_base: &GraphType, selection: TSelection) -> bool
+    where
+        TSelection: Fn(<GraphType as rs_graph::traits::GraphType>::Node) -> bool,
+    {
+        // Fix bug in `rs_graph::algorithms::is_connected`
+        let is_connected = |graph: &GraphType| -> bool {
+            graph.num_nodes() <= 1 || rs_graph::algorithms::is_connected(graph)
+        };
+
+        [
+            Self::get_subgraph(graph_base, &selection),
+            Self::get_subgraph(graph_base, &|node| !selection(node)),
+        ]
+        .iter()
+        .all(is_connected)
+    }
+}
