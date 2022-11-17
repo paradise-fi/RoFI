2a4da0cec91d737565cffb5991c5a44cee075448
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2022-11-17 01:26:23 +0100

    tools: Add reverse voxel convert

--- tools/convert/voxel.hpp
+++ tools/convert/voxel.hpp
@@ -1,5 +1,9 @@
+#pragma once
+
 #include <concepts>
+#include <map>
 #include <optional>
+#include <string_view>
 #include <vector>
 
 #include <nlohmann/json.hpp>
@@ -7,7 +11,10 @@
 #include <magic_enum.hpp>
 
 #include "atoms/result.hpp"
+#include "atoms/units.hpp"
+#include "atoms/unreachable.hpp"
 #include "configuration/rofiworld.hpp"
+#include "configuration/universalModule.hpp"
 
 
 namespace rofi::voxel
@@ -37,6 +44,12 @@ inline auto toIntegerArray( const rofi::configuration::matrices::Vector & values
     }
     return result;
 }
+inline auto toMatrixVector( const std::array< int, 3 > & position, double w = 0. )
+        -> rofi::configuration::matrices::Vector
+{
+    auto [ x, y, z ] = position;
+    return { double( x ), double( y ), double( z ), w };
+}
 
 using Position = std::array< int, 3 >;
 
@@ -51,6 +64,10 @@ inline auto toAxis( std::underlying_type_t< Axis > i ) -> Axis
     assert( axis );
     return axis ? *axis : throw std::logic_error( "Axis index is out-of-bounds" );
 }
+inline auto fromAxis( Axis axis ) -> std::underlying_type_t< Axis >
+{
+    return static_cast< std::underlying_type_t< Axis > >( axis );
+}
 inline auto nextAxis( Axis axis ) -> Axis
 {
     constexpr auto axes_values = magic_enum::enum_values< Axis >();
@@ -59,6 +76,15 @@ inline auto nextAxis( Axis axis ) -> Axis
     auto axis_index = magic_enum::enum_index( axis );
     assert( axis_index );
     return axes_values[ ( *axis_index + 1 ) % axes_values.size() ];
+}
+inline auto prevAxis( Axis axis ) -> Axis
+{
+    constexpr auto axes_values = magic_enum::enum_values< Axis >();
+    static_assert( axes_values.size() > 0 );
+
+    auto axis_index = magic_enum::enum_index( axis );
+    assert( axis_index );
+    return axes_values[ ( *axis_index + axes_values.size() - 1 ) % axes_values.size() ];
 }
 
 struct Direction {
@@ -92,6 +118,19 @@ struct Direction {
         return std::nullopt;
     }
 
+    auto movePosition( Position position ) const -> Position
+    {
+        position[ fromAxis( axis ) ] += is_positive ? 1 : -1;
+        return position;
+    }
+
+    auto opposite() const -> Direction
+    {
+        return Direction{ .axis = axis, .is_positive = !is_positive };
+    }
+
+    auto operator<=>( const Direction & ) const = default;
+
     Axis axis = {};
     bool is_positive = {};
 };
@@ -114,7 +153,7 @@ inline auto originalJointPosFromAngle( Angle angle ) -> std::optional< JointPosi
     }
     return std::nullopt;
 }
-auto jointPositionToInteger( JointPosition joint_pos ) -> int8_t
+inline auto jointPositionToInteger( JointPosition joint_pos ) -> int8_t
 {
     switch ( joint_pos ) {
         case JointPosition::Zero:
@@ -156,6 +195,11 @@ inline auto jointPositionInverse( JointPosition value ) -> JointPosition
 ///     when `joint_pos` is `Plus90` the Z-connector is towards the plus axis
 ///     otherwise the Z-connector is towards the minus axis
 struct Voxel {
+    /**
+     * Rofi Module -> Voxel
+     */
+
+private:
     static auto getVoxelPos( const rofi::configuration::matrices::Matrix & bodyPos )
             -> atoms::Result< Position >
     {
@@ -233,6 +277,8 @@ struct Voxel {
                                            .is_shoe_rotated = *isShoeRotated,
                                            .joint_pos = *jointPos } );
     }
+
+public:
     static auto fromRofiModule( const rofi::configuration::UniversalModule & rofiModule )
             -> atoms::Result< std::array< Voxel, 2 > >
     {
@@ -248,6 +294,196 @@ struct Voxel {
         return atoms::result_value( std::array{ *shoeA, *shoeB } );
     }
 
+
+    /**
+     * Voxel -> Rofi Module
+     */
+
+public:
+    auto getOtherBodyPos() const -> Position
+    {
+        return other_body_dir.movePosition( pos );
+    }
+
+    auto zConnDirection() const -> Direction
+    {
+        auto zConnAxis = is_shoe_rotated ? nextAxis( other_body_dir.axis )
+                                         : prevAxis( other_body_dir.axis );
+        switch ( joint_pos ) {
+            case JointPosition::Zero:
+                return other_body_dir.opposite();
+            case JointPosition::Plus90:
+                return Direction{ .axis = zConnAxis, .is_positive = true };
+            case JointPosition::Minus90:
+                return Direction{ .axis = zConnAxis, .is_positive = false };
+        }
+        ROFI_UNREACHABLE( "Joint Position is out of bounds" );
+    }
+    auto xPlusConnDirection() const -> Direction
+    {
+        if ( is_shoe_rotated ) {
+            return Direction{ .axis = prevAxis( other_body_dir.axis ),
+                              .is_positive = !other_body_dir.is_positive };
+        } else {
+            return Direction{ .axis = nextAxis( other_body_dir.axis ),
+                              .is_positive = other_body_dir.is_positive };
+        }
+    }
+
+    auto getXPlusConnMatrixRotation() const -> rofi::configuration::matrices::Matrix
+    {
+        namespace matrices = rofi::configuration::matrices;
+
+        auto getYPlusDir = []( Direction xPlusDir, Direction zPlusDir ) {
+            auto defaultIsPositive = xPlusDir.is_positive == zPlusDir.is_positive;
+            switch ( zPlusDir.axis ) {
+                case Axis::X:
+                    switch ( xPlusDir.axis ) {
+                        case Axis::Y:
+                            return Direction{ .axis = Axis::Z, .is_positive = defaultIsPositive };
+                        case Axis::Z:
+                            return Direction{ .axis = Axis::Y, .is_positive = !defaultIsPositive };
+                        case Axis::X:
+                            ROFI_UNREACHABLE(
+                                    "Z+ and X- connectors cannot have the same dir axis" );
+                    }
+                    ROFI_UNREACHABLE( "Invalid axis" );
+                case Axis::Y:
+                    switch ( xPlusDir.axis ) {
+                        case Axis::Z:
+                            return Direction{ .axis = Axis::X, .is_positive = defaultIsPositive };
+                        case Axis::X:
+                            return Direction{ .axis = Axis::Z, .is_positive = !defaultIsPositive };
+                        case Axis::Y:
+                            ROFI_UNREACHABLE(
+                                    "Z+ and X- connectors cannot have the same dir axis" );
+                    }
+                    ROFI_UNREACHABLE( "Invalid axis" );
+                case Axis::Z:
+                    switch ( xPlusDir.axis ) {
+                        case Axis::X:
+                            return Direction{ .axis = Axis::Y, .is_positive = defaultIsPositive };
+                        case Axis::Y:
+                            return Direction{ .axis = Axis::X, .is_positive = !defaultIsPositive };
+                        case Axis::Z:
+                            ROFI_UNREACHABLE(
+                                    "Z+ and X- connectors cannot have the same dir axis" );
+                    }
+                    ROFI_UNREACHABLE( "Invalid axis" );
+            }
+            ROFI_UNREACHABLE( "Invalid axis" );
+        };
+
+        auto xPlusDir = xPlusConnDirection();
+        auto zPlusDir = zConnDirection().opposite();
+        auto yPlusDir = getYPlusDir( xPlusDir, zPlusDir );
+
+        auto plusDirs = std::array{ xPlusDir, yPlusDir, zPlusDir };
+
+        auto result = matrices::identity;
+        for ( size_t i = 0; i < 3; i++ ) {
+            result.col( i ) = toMatrixVector( plusDirs[ i ].movePosition( {} ), 0. );
+        }
+        return result;
+    }
+
+    struct Connector {
+        std::string_view name;
+        Position pos;
+        Direction dir;
+        Direction oriVec;
+    };
+
+    static auto getConnectors( Voxel shoeA, Voxel shoeB ) -> std::array< Connector, 6 >
+    {
+        using namespace std::string_view_literals;
+
+        auto xPlusConnDirShoeA = shoeA.xPlusConnDirection();
+        auto xPlusConnDirShoeB = shoeB.xPlusConnDirection();
+
+        auto zConnDirShoeA = shoeA.zConnDirection();
+        auto zConnDirShoeB = shoeB.zConnDirection();
+
+        return std::array{
+                Connector{ .name = "A-X"sv,
+                           .pos = shoeA.pos,
+                           .dir = xPlusConnDirShoeA.opposite(),
+                           .oriVec = zConnDirShoeA.opposite() },
+                Connector{ .name = "A+X"sv,
+                           .pos = shoeA.pos,
+                           .dir = xPlusConnDirShoeA,
+                           .oriVec = zConnDirShoeA },
+                Connector{ .name = "A-Z"sv,
+                           .pos = shoeA.pos,
+                           .dir = zConnDirShoeA,
+                           .oriVec = xPlusConnDirShoeA },
+                Connector{ .name = "B-X"sv,
+                           .pos = shoeB.pos,
+                           .dir = xPlusConnDirShoeB.opposite(),
+                           .oriVec = zConnDirShoeB.opposite() },
+                Connector{ .name = "B+X"sv,
+                           .pos = shoeB.pos,
+                           .dir = xPlusConnDirShoeB,
+                           .oriVec = zConnDirShoeB },
+                Connector{ .name = "B-Z"sv,
+                           .pos = shoeB.pos,
+                           .dir = zConnDirShoeB,
+                           .oriVec = xPlusConnDirShoeB },
+        };
+    }
+
+    static auto getConnOrientation( Direction connDirection,
+                                    Direction connOrientationVector,
+                                    Direction otherOrientationVector )
+            -> rofi::configuration::roficom::Orientation
+    {
+        if ( connOrientationVector.axis == otherOrientationVector.axis ) {
+            if ( connOrientationVector.is_positive == otherOrientationVector.is_positive ) {
+                return rofi::configuration::roficom::Orientation::North;
+            } else {
+                return rofi::configuration::roficom::Orientation::South;
+            }
+        } else {
+            if ( connDirection.is_positive
+                 ^ ( nextAxis( connDirection.axis ) == connOrientationVector.axis )
+                 ^ ( connOrientationVector.is_positive == otherOrientationVector.is_positive ) )
+            {
+                return rofi::configuration::roficom::Orientation::West;
+            } else {
+                return rofi::configuration::roficom::Orientation::East;
+            }
+        }
+    }
+
+    static auto toRofiModule( Voxel shoeA, Voxel shoeB, rofi::configuration::ModuleId moduleId )
+            -> rofi::configuration::UniversalModule
+    {
+        auto getGamma = [ & ] {
+            if ( shoeA.is_shoe_rotated == shoeB.is_shoe_rotated ) {
+                return 0_deg;
+            }
+            if ( shoeA.is_shoe_rotated ) {
+                assert( !shoeB.is_shoe_rotated );
+                return 90_deg;
+            }
+            assert( !shoeA.is_shoe_rotated );
+            assert( shoeB.is_shoe_rotated );
+            return -90_deg;
+        };
+
+        assert( shoeA.other_body_dir.movePosition( shoeA.pos ) == shoeB.pos );
+        assert( shoeB.other_body_dir.movePosition( shoeB.pos ) == shoeA.pos );
+
+        auto alpha = Angle::deg( jointPositionToInteger( shoeA.joint_pos ) );
+        auto beta = Angle::deg( jointPositionToInteger( shoeB.joint_pos ) );
+        auto gamma = getGamma();
+
+        return rofi::configuration::UniversalModule( moduleId, alpha, beta, gamma );
+    }
+
+public:
+    bool operator==( const Voxel & ) const = default;
+
     Position pos = {};
     Direction other_body_dir = {};
     bool is_shoe_rotated = {};
@@ -255,6 +491,12 @@ struct Voxel {
 };
 
 struct VoxelWorld {
+private:
+    // Mapping (connectorPos, connectorDir) -> (component, orientationVector)
+    using ConnectorMap = std::map< std::pair< Position, Direction >,
+                                   std::pair< rofi::configuration::Component, Direction > >;
+
+public:
     static auto fromRofiWorld( const rofi::configuration::RofiWorld & rofiWorld )
             -> atoms::Result< VoxelWorld >
     {
@@ -262,24 +504,6 @@ struct VoxelWorld {
             return atoms::result_error< std::string >(
                     "RofiWorld has to be prepared to convert to voxel json" );
         }
-
-        constexpr auto subPos = []( Position lhs, const Position & rhs ) -> Position {
-            for ( size_t i = 0; i < lhs.size(); i++ ) {
-                lhs[ i ] -= rhs[ i ];
-            }
-            return lhs;
-        };
-
-        auto beginPos = std::optional< Position >();
-        auto updateLimitPos = [ &beginPos ]( const Position & pos ) {
-            if ( !beginPos ) {
-                beginPos = pos;
-                return;
-            }
-            for ( size_t i = 0; i < pos.size(); i++ ) {
-                ( *beginPos )[ i ] = std::min( ( *beginPos )[ i ], pos[ i ] );
-            }
-        };
 
         auto voxelBodies = std::vector< Voxel >();
         for ( const auto & rofiModule : rofiWorld.modules() ) {
@@ -293,39 +517,153 @@ struct VoxelWorld {
                 return std::move( voxelModule ).assume_error_result();
             }
             for ( auto voxel : *voxelModule ) {
-                updateLimitPos( voxel.pos );
                 voxelBodies.push_back( std::move( voxel ) );
             }
-        }
-        for ( auto & voxel : voxelBodies ) {
-            assert( beginPos && "Position has to be updated before adding the voxel body" );
-            voxel.pos = subPos( std::move( voxel.pos ), *beginPos );
         }
 
         return atoms::result_value( VoxelWorld{ .bodies = std::move( voxelBodies ) } );
     }
 
+private:
+    static auto isModuleRepr( Voxel voxelBody ) -> bool
+    {
+        return voxelBody.other_body_dir.is_positive;
+    }
+
+    auto posToBodiesMap() const -> atoms::Result< std::map< Position, Voxel > >
+    {
+        auto bodiesMap = std::map< Position, Voxel >();
+        for ( const auto & voxelBody : bodies ) {
+            auto inserted = bodiesMap.emplace( voxelBody.pos, voxelBody );
+            if ( !inserted.second ) {
+                return atoms::result_error< std::string >( "Multiple bodies at the same position" );
+            }
+        }
+        return atoms::result_value( std::move( bodiesMap ) );
+    }
+
+    static void connectModules( ConnectorMap & connectorMap )
+    {
+        for ( const auto & [ connPosDir, connWithOri ] : connectorMap ) {
+            const auto & [ connPos, connDir ] = connPosDir;
+            if ( !connDir.is_positive ) {
+                // Select each connection only once
+                continue;
+            }
+
+            auto otherConnDirPos = std::pair{ connDir.movePosition( connPos ), connDir.opposite() };
+            auto otherConnWithOri = connectorMap.find( otherConnDirPos );
+            if ( otherConnWithOri == connectorMap.end() ) {
+                continue;
+            }
+
+            const auto & [ conn, connOri ] = connWithOri;
+            const auto & [ otherConn, otherConnOri ] = otherConnWithOri->second;
+
+            assert( connDir.axis != connOri.axis );
+            assert( connDir.axis != otherConnOri.axis );
+
+            auto orientation = Voxel::getConnOrientation( connDir, connOri, otherConnOri );
+            rofi::configuration::connect( conn, otherConn, orientation );
+        }
+    }
+
+    auto fixInSpace( ConnectorMap & connectorMap ) const -> atoms::Result< std::monostate >
+    {
+        auto firstBody = bodies.front();
+        auto firstConnIt = connectorMap.find(
+                std::pair{ firstBody.pos, firstBody.xPlusConnDirection().opposite() } );
+        if ( firstConnIt == connectorMap.end() ) {
+            return atoms::result_error< std::string >( "Couldn't find first body X- connector" );
+        }
+        auto firstConn = firstConnIt->second;
+        auto refPoint = toMatrixVector( firstBody.pos );
+        auto rotation = firstBody.getXPlusConnMatrixRotation();
+
+        rofi::configuration::connect< rofi::configuration::RigidJoint >( firstConn.first,
+                                                                         refPoint,
+                                                                         rotation );
+        return atoms::result_value( std::monostate{} );
+    }
+
+public:
+    auto toRofiWorld() const -> atoms::Result< std::shared_ptr< rofi::configuration::RofiWorld > >
+    {
+        if ( bodies.empty() ) {
+            return atoms::result_error< std::string >( "VoxelWorld cannot be empty" );
+        }
+
+        auto bodiesMapResult = posToBodiesMap();
+        if ( !bodiesMapResult ) {
+            return std::move( bodiesMapResult ).assume_error_result();
+        }
+        const auto bodiesMap = std::move( *bodiesMapResult );
+        auto moduleId = rofi::configuration::ModuleId( 1 );
+        auto connectorMap = ConnectorMap();
+
+        auto result = std::make_shared< rofi::configuration::RofiWorld >();
+        for ( const auto & voxelBody : bodies ) {
+            if ( !isModuleRepr( voxelBody ) ) {
+                continue;
+            }
+
+            if ( !bodiesMap.contains( voxelBody.getOtherBodyPos() ) ) {
+                return atoms::result_error< std::string >( "Invalid world" );
+            }
+            auto shoeA = voxelBody;
+            auto shoeB = bodiesMap.at( voxelBody.getOtherBodyPos() );
+
+            auto & mod = result->insert( Voxel::toRofiModule( shoeA, shoeB, moduleId++ ) );
+
+            for ( auto conn : Voxel::getConnectors( shoeA, shoeB ) ) {
+                auto inserted = connectorMap.emplace( std::pair{ conn.pos, conn.dir },
+                                                      std::pair{ mod.getConnector( conn.name ),
+                                                                 conn.oriVec } );
+                if ( !inserted.second ) {
+                    return atoms::result_error< std::string >(
+                            "Multiple connectors at the same position and direction" );
+                }
+            }
+        }
+
+        connectModules( connectorMap );
+
+        auto fixInSpaceResult = fixInSpace( connectorMap );
+        if ( !fixInSpaceResult ) {
+            return std::move( fixInSpaceResult ).assume_error_result();
+        }
+
+        if ( auto prepared = result->validate(); !prepared ) {
+            return std::move( prepared ).assume_error_result();
+        }
+        return atoms::result_value( std::move( result ) );
+    }
+
+    bool operator==( const VoxelWorld & ) const = default;
+
+
+public:
     std::vector< Voxel > bodies = {};
 };
 
 
 // Make Axis (de)serializable to nlohmann::json
-void to_json( nlohmann::json & j, Axis axis )
+inline void to_json( nlohmann::json & j, Axis axis )
 {
     j = magic_enum::enum_name( axis );
 }
-void from_json( const nlohmann::json & j, Axis & axis )
+inline void from_json( const nlohmann::json & j, Axis & axis )
 {
     auto value = magic_enum::enum_cast< Axis >( j.get< std::string >() );
     axis = value ? *value : throw std::runtime_error( "Could not read Axis" );
 }
 
 // Make JointPosition (de)serializable to nlohmann::json
-void to_json( nlohmann::json & j, const JointPosition & joint_pos )
+inline void to_json( nlohmann::json & j, const JointPosition & joint_pos )
 {
     j = jointPositionToInteger( joint_pos );
 }
-void from_json( const nlohmann::json & j, JointPosition & joint_pos )
+inline void from_json( const nlohmann::json & j, JointPosition & joint_pos )
 {
     auto value = j.get< int >();
     switch ( value ) {
