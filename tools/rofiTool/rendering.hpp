95c5c5a61f3cf0f5c9c6dbba428f7be902a4f112
Jaroměl Tuzerád <jarom@DESKTOP-41I8BHT>
2022-11-25 21:32:52 +0100

    rofiTool: points cmd uses PCA coordinate system

--- tools/rofiTool/rendering.hpp
+++ tools/rofiTool/rendering.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <configuration/rofiworld.hpp>
-#include <isoreconfig/isomorphic.hpp>
 
 void renderConfiguration( rofi::configuration::RofiWorld world, const std::string& configName = "unknown" );
 void renderPoints( rofi::configuration::RofiWorld world, const std::string& configName = "unknown", bool showModules = false );
