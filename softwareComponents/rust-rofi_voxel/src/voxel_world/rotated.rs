9984b721a060b6120a051b08dd245180e43edc2e
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2023-01-08 18:29:21 +0100

    voxel: Move dump_bodies to Debug trait

--- softwareComponents/rust-rofi_voxel/src/voxel_world/rotated.rs
+++ softwareComponents/rust-rofi_voxel/src/voxel_world/rotated.rs
@@ -34,6 +34,39 @@ pub struct RotatedVoxelWorld<'a> {
     world: CenteredVoxelWorld<'a>,
 }
 
+impl<'a> std::fmt::Debug for RotatedVoxelWorld<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let indent = if f.alternate() { "    " } else { "" };
+        let ws_sep = if f.alternate() { "\n" } else { " " };
+        f.write_str("RotatedVoxelWorld {")?;
+        f.write_str(ws_sep)?;
+
+        f.write_fmt(format_args!(
+            "{indent}center: {:?},{ws_sep}",
+            self.world.center()
+        ))?;
+
+        f.write_fmt(format_args!(
+            "{indent}rotation: {:?},{ws_sep}",
+            self.rotation
+        ))?;
+
+        f.write_fmt(format_args!(
+            "{indent}sizes: {:?},{ws_sep}",
+            self.size_ranges()
+        ))?;
+
+        f.write_fmt(format_args!("{indent}bodies: "))?;
+        super::debug_fmt_bodies(self.all_bodies(), f, ws_sep, indent, indent, f.alternate())?;
+
+        if f.alternate() {
+            f.write_str(",")?;
+        }
+        f.write_str(ws_sep)?;
+        f.write_str("}")
+    }
+}
+
 impl<'a> RotatedVoxelWorld<'a> {
     pub fn new(world: CenteredVoxelWorld<'a>, rotation: atoms::Rotation) -> Self {
         Self { rotation, world }
@@ -71,18 +104,6 @@ impl<'a> RotatedVoxelWorld<'a> {
             let rot_pos = self.get_rot_pos(rel_pos);
             (rot_body, rot_pos)
         })
-    }
-
-    pub fn dump_bodies(&self) {
-        eprintln!(
-            "RotatedVoxelWorld {{ center={:?}, rotation={:?}, bodies=[",
-            self.world.center(),
-            self.rotation
-        );
-        for (body, pos) in self.all_bodies() {
-            eprintln!("    {pos:?}: {body:?},");
-        }
-        eprintln!("] }}");
     }
 
     pub fn combine_with(&self, other: &CenteredVoxelWorld) -> (VoxelWorld, VoxelPos) {
