75a2033fc528e7de02189ec8164c80394e958f65
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2023-01-23 16:54:53 +0100

    voxel: Update rofi-voxel

--- releng/cmake/RmakeUtils.cmake
+++ releng/cmake/RmakeUtils.cmake
@@ -1,14 +1,13 @@
 function(add_executable_script SCRIPT_PATH)
+    set(FLAG_KEYWORDS BASH)
     set(MULTI_VALUE_KEYWORDS DEPENDS)
-    cmake_parse_arguments(SCR "" "" "${MULTI_VALUE_KEYWORDS}" ${ARGN})
+    cmake_parse_arguments(SCR "${FLAG_KEYWORDS}" "" "${MULTI_VALUE_KEYWORDS}" ${ARGN})
     get_filename_component(filename "${SCRIPT_PATH}" NAME)
     get_filename_component(abspath "${SCRIPT_PATH}" ABSOLUTE)
 
     add_custom_command(
         OUTPUT
             "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${filename}"
-        COMMAND
-            bash -n "${abspath}"
         COMMAND
             ${CMAKE_COMMAND} -E make_directory "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
         COMMAND
@@ -19,4 +18,11 @@ function(add_executable_script SCRIPT_PATH)
             "${abspath}" ${SCR_DEPENDS}
     )
     add_custom_target("${filename}" ALL DEPENDS "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${filename}")
+    if(SCR_BASH)
+        add_custom_command(
+            TARGET "${filename}"
+            PRE_BUILD
+            COMMAND bash -n "${abspath}"
+        )
+    endif()
 endfunction()
