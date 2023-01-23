15f3ee1948691e76009dca437782247cca5b885c
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2023-01-23 16:56:40 +0100

    voxel: Add experiment generators and snake rand experiments

---
+++ experiments/voxel/enviroment.Dockerfile
@@ -0,0 +1,23 @@
+FROM ghcr.io/paradise-fi/rofi.debian:latest
+
+SHELL ["/bin/bash", "-c"]
+
+RUN cd /; \
+    git clone https://github.com/paradise-fi/RoFI.git --depth 1 --branch master; \
+    cd RoFI; \
+    source ./setup.sh Release; \
+    rcfg desktop; \
+    rmake rofi-voxel
+
+# Form an entrypoint that setups environment
+RUN echo $'#!/usr/bin/env bash\n\
+    source ./setup.sh -s Release;\n\
+    util/monitorMemUsage.sh /artefact/results.json $@\n' > /bin/rofiInvoke; \
+    chmod +x /bin/rofiInvoke
+
+# Uncomment if you want to enable debug assertions in Rust
+# RUN export RUSTFLAGS="-C debug-assertions -C overflow-checks"
+
+WORKDIR /RoFI
+
+ENTRYPOINT ["rofiInvoke"]
---
+++ experiments/voxel/gen_tasks_matrix.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import itertools
+import subprocess
+import sys
+import click
+import json
+from typing import Any, Dict, Iterable, List, TextIO
+
+
+def safe_as_arg(arg: Any) -> str:
+    return subprocess.list2cmdline([str(arg)])
+
+
+# Json formatted dump with newline at the end
+def dump_json(value: Any, file: TextIO, *, indent: int = 4):
+    json.dump(value, fp=file, indent=indent)
+    print(file=file)
+
+
+def all_combinations(matrix: Dict[str, List[Any]]) -> Iterable[Dict[str, Any]]:
+    pair_matrix = [[(name, value) for value in matrix[name]]
+                   for name in matrix]
+    return [dict(pairs) for pairs in itertools.product(*pair_matrix)]
+
+
+@click.command(help='Generate task set from given format and args from TASK_ARGS_FILE (in json format) and cmd and matrix args from CMD_ARGS_FILE')
+@click.argument('task_args_file', type=click.File())
+@click.argument('cmd_args_file', type=click.File())
+@click.option('--raw-args', is_flag=True,
+              help='Pass the args as-is')
+def gen_tasks_matrix(task_args_file: TextIO, cmd_args_file: TextIO, raw_args: bool):
+    task_args: List[Dict[str, Any]] = json.load(task_args_file)
+    assert isinstance(task_args, list)
+    assert all([isinstance(args, dict) for args in task_args])
+
+    cmd_args: Dict[str, Any] = json.load(cmd_args_file)
+    assert isinstance(cmd_args, dict)
+    cmd: str = cmd_args['cmd']
+    assert isinstance(cmd, str)
+
+    matrix_args: Dict[str, List[Any]] = cmd_args['args']
+    assert isinstance(matrix_args, dict)
+    assert all([isinstance(matrix_args[arg], list) for arg in matrix_args])
+
+    if not raw_args:
+        task_args = [{name: safe_as_arg(args[name]) for name in args}
+                     for args in task_args]
+        matrix_args = {name: [safe_as_arg(value) for value in matrix_args[name]]
+                       for name in matrix_args}
+
+    tasks = [cmd.format_map(margs | targs)
+             for targs in task_args for margs in all_combinations(matrix_args)]
+    dump_json(tasks, sys.stdout)
+
+
+if __name__ == '__main__':
+    gen_tasks_matrix()
