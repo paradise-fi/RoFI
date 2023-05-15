e97a39034954298126662fc77c6eb7e5b855cf92
pitiK3U <djhlinacz@gmail.com>
2023-05-15 14:05:27 +0200

    roficom: Add lidar library implementation

---
+++ hardwareModules/RoFICoM/software/control_board/vl53l1_lidar/stm32cxx.config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#define STM32CXX_USE_MEMORY_POOL
+#define STM32CXX_MEMORY_BUCKETS \
+    memory::Bucket< 1025, 5 >, \
+    memory::Bucket< 128, 5 >, \
+    memory::Bucket< 64, 10 >, \
+    memory::Bucket< 32, 20 >
+
+#define STM32CXX_DBG_ALLOCATOR memory::Pool
