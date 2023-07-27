b346890891237ad894559642dfe6fd082d398045
Jakub Andrysek <jakub.andrysek03@gmail.com>
2023-07-27 10:26:11 +0200

    Add gridui as dependency

--- softwareComponents/rofiHalGridUi/src/RBControl_wifi.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Compatibility header
-#warning "The RBControl_wifi.hpp header is deprecated, please use rbwifi.h from the RBProtocol library.h"
-
-#include "rbwifi.h"
--- softwareComponents/rofiHalGridUi/src/gridui_version.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define RB_GRIDUI_VERSION 0x041101
-
