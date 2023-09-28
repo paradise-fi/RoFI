fe6f45e37932aa0f1d3f0ec5f2fc2d97f1d18b2e
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2023-09-28 11:39:20 +0200

    experiments: Make task generation reusable

--- experiments/voxel/gen_tasks_matrix.py
+++ experiments/voxel/gen_tasks_matrix.py
@@ -6,11 +6,15 @@ import json
 import subprocess
 import sys
 
+from dataclasses import dataclass
 from typing import Any, Iterable, TextIO, TypeVar, Union
+from utils import OneOrMore, parse_json
 
 
-T = TypeVar("T")
-OneOrMore = Union[list[T], T]
+@dataclass(kw_only=True)
+class ArgsFile:
+    cmd: str
+    args: OneOrMore[dict[str, OneOrMore[Any]]]
 
 
 def safe_as_arg(arg: Any) -> str:
@@ -28,23 +32,9 @@ def all_combinations(matrix: dict[str, list[Any]]) -> Iterable[dict[str, Any]]:
     return [dict(pairs) for pairs in itertools.product(*pair_matrix)]
 
 
-@click.command(
-    help="Generate task set from given format and args from TASK_ARGS_FILE (in json format) and cmd and matrix args from CMD_ARGS_FILE"
-)
-@click.argument("task_args_file", type=click.File())
-@click.argument("cmd_args_file", type=click.File())
-@click.option("--raw-args", is_flag=True, help="Pass the args as-is")
-def gen_tasks_matrix(task_args_file: TextIO, cmd_args_file: TextIO, raw_args: bool):
-    task_args: list[dict[str, Any]] = json.load(task_args_file)
-    assert isinstance(task_args, list)
-    assert all([isinstance(args, dict) for args in task_args])
-
-    cmd_args: dict[str, Any] = json.load(cmd_args_file)
-    assert isinstance(cmd_args, dict)
-    cmd: str = cmd_args["cmd"]
-    assert isinstance(cmd, str)
-
-    matrix_args_list: OneOrMore[dict[str, OneOrMore[Any]]] = cmd_args["args"]
+def all_arg_combinations(
+    matrix_args_list: OneOrMore[dict[str, OneOrMore[Any]]], *, escape_args: bool
+) -> list[dict[str, Any]]:
     if not isinstance(matrix_args_list, list):
         matrix_args_list = [matrix_args_list]
     for matrix_args in matrix_args_list:
@@ -53,10 +43,7 @@ def gen_tasks_matrix(task_args_file: TextIO, cmd_args_file: TextIO, raw_args: bo
             if not isinstance(matrix_args[key], list):
                 matrix_args[key] = [matrix_args[key]]
 
-    if not raw_args:
-        task_args = [
-            {name: safe_as_arg(args[name]) for name in args} for args in task_args
-        ]
+    if escape_args:
         matrix_args_list = [
             {
                 key: [safe_as_arg(value) for value in matrix_args[key]]
@@ -65,11 +52,36 @@ def gen_tasks_matrix(task_args_file: TextIO, cmd_args_file: TextIO, raw_args: bo
             for matrix_args in matrix_args_list
         ]
 
-    tasks = [
-        cmd.format_map(margs | targs)
-        for targs in task_args
+    return [
+        margs
         for matrix_args in matrix_args_list
         for margs in all_combinations(matrix_args)
+    ]
+
+
+@click.command(
+    help="Generate task set from given format and args from TASK_ARGS_FILE (in json format) and cmd and matrix args from CMD_ARGS_FILE"
+)
+@click.argument("task_args_file", type=click.File())
+@click.argument("cmd_args_file", type=click.File())
+@click.option(
+    "--escape-args",
+    is_flag=True,
+    help="Pass each arg as one argument to underlying command",
+)
+def gen_tasks_matrix(task_args_file: TextIO, cmd_args_file: TextIO, escape_args: bool):
+    task_args = parse_json(list[dict[str, Any]], json.load(task_args_file))
+    cmd_args = parse_json(ArgsFile, json.load(cmd_args_file))
+
+    if escape_args:
+        task_args = [
+            {name: safe_as_arg(args[name]) for name in args} for args in task_args
+        ]
+
+    tasks = [
+        cmd_args.cmd.format_map(margs | targs)
+        for targs in task_args
+        for margs in all_arg_combinations(cmd_args.args, escape_args=escape_args)
     ]
     dump_json(tasks, sys.stdout)
 
