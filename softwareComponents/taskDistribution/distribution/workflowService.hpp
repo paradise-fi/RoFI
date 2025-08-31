b7b0fe3e8b02ff9f24cc1767a7af74984b9f1b2e
xvavrek <matvav13@gmail.com>
2025-08-31 16:30:34 +0200

    Moved doWork into workflowService

---
+++ softwareComponents/taskDistribution/distribution/workflowService.hpp
@@ -0,0 +1,88 @@
+#include "functionRegistry.hpp"
+#include "messageSender.hpp"
+
+class WorkFlowService
+{
+    MessageSender& _sender;
+    FunctionRegistry& _functionRegistry;
+
+    void DistributeTask( FunctionDistributionType distributionType, TaskBase& task, 
+        int method_id, const Ip6Addr& requester )
+    {
+        switch ( distributionType )
+        {
+            case FunctionDistributionType::Broadcast: 
+            {
+                _sender.broadcastMessage( DistributionMessageType::TaskAssignment, task, method_id );
+                return;
+            }
+
+            case FunctionDistributionType::Unicast:
+            {
+                _sender.sendMessage( DistributionMessageType::TaskAssignment, task, requester );
+                return;
+            }
+
+            default:
+            {
+                std::cout << "Undefined Function Distribution Type found." << std::endl;
+                return;
+            }
+        }
+    }
+public:
+    WorkFlowService(MessageSender& sender, FunctionRegistry& functionRegistry)
+    : _sender( sender ), _functionRegistry( functionRegistry ){}
+
+    void doWorkLeader( int method_id )
+    {
+        if ( !_functionRegistry.anyTaskRequests() )
+        {
+            return;
+        }
+
+        auto requester = _functionRegistry.getTaskRequester();
+        if ( !requester.has_value() )
+        {
+            return;
+        }
+
+        auto task = _functionRegistry.popTaskForAddress( requester.value(), true );
+        if ( !task.has_value() )
+        {
+            std::cout << "Task distribution failed: No initial task given.";
+            return;
+        }
+
+        auto function = _functionRegistry.getFunction( task.value().get().functionId() );
+        if ( !function.has_value() )
+        {
+            std::cout << "Task distribution failed: Given task has associated no function.";
+            return;
+        }
+
+        DistributeTask( function.value().get().distributionType(), task.value().get(), method_id, requester.value() );
+    }
+
+    void doWorkFollower( const Ip6Addr& address, const Ip6Addr& leader )
+    {
+        auto taskCandidate = _functionRegistry.popTaskForAddress( address );
+
+        if ( !taskCandidate.has_value() )
+        {
+            return;
+        }
+
+        auto task = std::move( taskCandidate.value() );
+        
+        if  ( !_functionRegistry.invokeFunction( task.get() ) )
+        {
+            _sender.sendMessage( DistributionMessageType::TaskFailed, task.get(), leader );
+            _functionRegistry.finishActiveTask( address );
+            return;
+        }
+
+        _sender.sendMessage( DistributionMessageType::TaskResult, task.get(), leader );
+        _functionRegistry.finishActiveTask( address );
+    }
+};
