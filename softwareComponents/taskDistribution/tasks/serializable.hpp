f9c8cc33d98665d0bbbfe9f9433524c5afd6cd83
xvavrek <matvav13@gmail.com>
2025-09-13 00:55:37 +0200

    Added documentation to serializable, fixed up some minor issues

--- softwareComponents/taskDistribution/tasks/serializable.hpp
+++ softwareComponents/taskDistribution/tasks/serializable.hpp
@@ -5,8 +5,16 @@
 #include <cstdint>
 
 struct Serializable {
+    /// @brief Copy the value of this structure into buffer. 
+    /// @param buffer The buffer, it is the responsibility of the serialize() implementation to move the pointer by the size of your data type.
     virtual void serialize( uint8_t*& buffer ) const = 0;
+
+    /// @brief Copy the value from the buffer into this structure.
+    /// @param buffer The buffer, it is the responsibility of the deserialize() implementation to move the pointer by the size of your data type.
     virtual void deserialize( const uint8_t*& buffer ) = 0;
+
+    /// @brief The size of your data type.
+    /// @return size_t of the size of your data type.
     virtual std::size_t size() const = 0;
 };
 
