91a3867e4992639e6ecb8d05c0ba5428c7a65a9a
xvavrek <matvav13@gmail.com>
2025-09-14 20:03:30 +0200

    Added functions for metadata handling in memory implementation

--- softwareComponents/taskDistribution/memory/sharedMemoryBase.hpp
+++ softwareComponents/taskDistribution/memory/sharedMemoryBase.hpp
@@ -11,7 +11,9 @@ class SharedMemoryBase {
         virtual void setLeader( Ip6Addr leader ) = 0;
         virtual void onStorageMessage( Ip6Addr sender, uint8_t* data, unsigned int size ) = 0;
         virtual bool store( uint8_t* data, int size, int address ) = 0;
-        virtual std::vector<uint8_t> read( int address ) = 0;
+        virtual std::vector< uint8_t > read( int address ) = 0;
         virtual bool remove( int address ) = 0;
+        virtual std::vector< uint8_t > readMetadata( int address, std::string key ) = 0;
+        virtual bool storeMetadata( int address, std::string key, uint8_t* metadata, int metadataSize ) = 0;
         virtual void clear() = 0;
 };
