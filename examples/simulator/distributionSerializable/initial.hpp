c30db449b7d39b3c788b6bf09f435c812a970322
xvavrek <matvav13@gmail.com>
2025-09-14 22:53:07 +0200

    Added example for using a serializable struct as an argument and result of a function

---
+++ examples/simulator/distributionSerializable/initial.hpp
@@ -0,0 +1,68 @@
+#include "distributionManager.hpp"
+#include "distributedFunction.hpp"
+#include "functionHandle.hpp"
+#include "naiveBarrier.hpp"
+#include "message.hpp"
+
+/// @brief The initial function is best used to handle distributed initialization. In this example, it does not do much
+/// in terms of initialization, but you may use it to, for example, gather information about the network.
+class Initial : public DistributedFunction< int >
+{
+    int _moduleId;
+    DistributionManager& _manager;
+
+public:
+    Initial( int moduleId, DistributionManager& manager )
+    : _moduleId( moduleId ),
+      _manager( manager ) {}
+
+    virtual FunctionResult< int > execute() override
+    {
+        return FunctionResult< int >( _moduleId, true );
+    }
+
+    virtual void onFunctionSuccess( std::optional< int > result, const Ip6Addr& origin ) override
+    {
+        if ( !result.has_value() )
+        {
+            std::cout << "No result value" << std::endl;
+            return;
+        }
+
+        int moduleId = result.value();
+
+        std::cout << "Initial ModuleId: " << moduleId << std::endl;
+
+        auto messageHandle = _manager.functionRegistry().getFunctionHandle< Message, Message >( 1 ).value();
+
+        if ( !messageHandle( origin, 1, false, std::tuple< Message >( Message( std::string( "Hello, follower." ) ) ) ) )
+        {
+            std::cout << "Execution of function " << functionName() << "failed." << std::endl;
+        }
+    }
+
+    virtual void onFunctionFailure( std::optional< int >, const Ip6Addr& ) override
+    {
+        return;
+    }
+
+    virtual std::string functionName() const override
+    {
+        return "Initial";
+    }
+
+    virtual int functionId() const override
+    {
+        return 0;
+    }
+
+    virtual FunctionCompletionType completionType() const override
+    {
+        return FunctionCompletionType::NonBlocking;
+    }
+
+    virtual FunctionDistributionType distributionType() const override
+    {
+        return FunctionDistributionType::Unicast;
+    }
+};
