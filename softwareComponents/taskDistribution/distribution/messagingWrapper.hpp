9357c1c50107dc31a630734f336950d0d9fbb698
xvavrek <matvav13@gmail.com>
2025-09-28 00:26:20 +0200

    Added arguemnt names to onMessageHandler in messagingWrapper

--- softwareComponents/taskDistribution/distribution/messagingWrapper.hpp
+++ softwareComponents/taskDistribution/distribution/messagingWrapper.hpp
@@ -20,7 +20,7 @@ public:
         Ip6Addr& address,
         u16_t distributionPort,
         MessageDistributor* distributor,
-        std::function< void( Ip6Addr, DistributionMessageType, uint8_t* data, unsigned int size ) > onMessageHandler,
+        std::function< void( Ip6Addr sender, DistributionMessageType messageType, uint8_t* data, unsigned int size ) > onMessageHandler,
         std::unique_ptr< udp_pcb > pcb )
     : _pcb( std::move( pcb ) ),
       _receiver( distributionPort, _pcb.get(), onMessageHandler ),
